84d5a67dc862e9dc28879ef3733224dc87dbda19
DorinR <dorin.rogov@gmail.com>
2019-09-04 13:58:23 -0400

    #2 added initial project setup for React client

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PolitisenseTentativeLogo from './PolitisenseTentativeLogo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={PolitisenseTentativeLogo} className='App-logo' alt='logo' />
+        <p>
+          <code>[Website Under Construction]</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
