4893b0297605767d1ecd6e2c113ab11f1cefe8c8
mathewplouffe <m_plo@encs.concordia.ca>
2019-09-13 20:43:11 -0400

    #25 [infrastructure/node] : added server.js

---
+++ build_scripts/build.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+fix_style () {
+	npx standard --fix
+}
+verbose_style_errors () {
+	npx standard -v
+}
+quiet_style_errors () {
+	for line in $((npx standard -v) 2>&1)
+	do
+		let style_errors=style_errors+1
+	done
+	echo "There are $style_errors style errors in this file"
+}
+style_error_logging () {
+	if [ "$1" = "--verbose" ]
+	then
+		verbose_style_errors
+	elif [ "$1" = "--quiet" ]
+	then
+		quiet_style_errors
+	fi
+}
+process_flags () {
+
+local flag_list=$@
+
+if [ -z $1 ]
+then
+	flag_list=( "--quiet" )
+fi
+for flag in $flag_list
+do
+	if [ "$flag" = "--strict" ]
+	then
+		echo "Strict style rules enabled, build will fail by style errors"		
+	fi
+	style_error_logging $flag
+	if [ "$flag" = "--fix" ]
+	then
+		style_errors=0
+		echo "Fixing style errors..."
+		fix_style
+	fi
+done
+}
+build () {
+	style_errors=0;
+	cd ../client
+	npm install
+	process_flags $@
+	npm test
+	npm build run
+	return $style_errors		
+}
+build $@
--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PolitisenseTentativeLogo from './PolitisenseTentativeLogo.png'
 import './App.css'
 
-function App () {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={PolitisenseTentativeLogo} className='App-logo' alt='logo' />
-        <p>
-          <code>[Website Under Construction]</code>
-        </p>
-      </header>
-    </div>
-  )
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={PolitisenseTentativeLogo} className='App-logo' alt='logo' />
+          <p>
+            <code>[Website Under Construction]</code>
+          </p>
+        </header>
+      </div>
+    )
+  }
 }
-
 export default App
