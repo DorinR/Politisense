b4c1bf1d3ff535fa5c08c80b58626ba89dc6533d
Refat Kamal <refat.k17@gmail.com>
2019-09-20 19:27:49 -0400

    #12 can get attribute data when needed

--- client/src/xmlDataParser/src/XmlDataParser.js
+++ client/src/xmlDataParser/src/XmlDataParser.js
@@ -1,17 +1,21 @@
-const cheerio = require('cheerio');
+const cheerio = require('cheerio')
 
 class XmlDataParser {
-    constructor(xml) {
-        this.xml = xml;
-        this.$ = cheerio.load(xml, {
-            normalizeWhitespace: true,
-            xmlMode: true
-        });
-    }
+  constructor (xml) {
+    this.xml = xml
+    this.$ = cheerio.load(xml, {
+      normalizeWhitespace: true,
+      xmlMode: true
+    })
+  }
 
-    getDataInTag(tag) {
-        return this.$(tag).text();
-    }
+  getDataInTag (tag) {
+    return this.$(tag).text()
+  }
+
+  getDataInAttribute (tag, attribute) {
+    return this.$(tag)[0].attribs[attribute]
+  }
 }
 
-module.exports.XmlDataParser = XmlDataParser;
+module.exports.XmlDataParser = XmlDataParser
--- client/src/xmlDataParser/test/XmlDataParser.test.js
+++ client/src/xmlDataParser/test/XmlDataParser.test.js
@@ -1,16 +1,21 @@
-import {XmlDataParser} from '../src/XmlDataParser'
+import { XmlDataParser } from '../src/XmlDataParser'
 
-import {assert} from 'chai';
-import {expect} from 'chai';
-import {should} from 'chai';
+import { assert } from 'chai'
+// import { expect } from 'chai'
+// import { should } from 'chai'
 
 describe('XmlDataParser', () => {
-    it('get text in xml', () => {
-        let xml = '<text>ANSWER</text>';
-        let parser = new XmlDataParser(xml);
-        let parserAns = parser.getDataInTag('text');
-        assert.strictEqual(parserAns, 'ANSWER')
-    });
-});
+  it('get text in xml', () => {
+    const xml = '<text>ANSWER</text>'
+    const parser = new XmlDataParser(xml)
+    const parserAns = parser.getDataInTag('text')
+    assert.strictEqual(parserAns, 'ANSWER')
+  })
 
-
+  it('get value of specified attribute', () => {
+    const xml = '<text attribute="attr">ANSWER</text>'
+    const parser = new XmlDataParser(xml)
+    const parserAns = parser.getDataInAttribute('text', 'attribute')
+    assert.strictEqual(parserAns, 'attr')
+  })
+})
