237acf76252758990b29123710b285a1cd30d420
mathewplouffe <m_plo@encs.concordia.ca>
2019-09-22 14:29:34 -0400

    #12 [infrastructure/firebase-tdg] : refactored Firebase.js to be a class

--- client/src/Firebase.js
+++ client/src/Firebase.js
@@ -1,15 +1,29 @@
-import * as firebase from 'firebase'
+const fb = require('firebase')
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyBdCSbXtHoTPO4JfPDicPhnams3q1p_6AQ',
-  authDomain: 'abdulla-2c3a5.firebaseapp.com',
-  databaseURL: 'https://abdulla-2c3a5.firebaseio.com',
-  projectId: 'abdulla-2c3a5',
-  storageBucket: 'abdulla-2c3a5.appspot.com',
-  messagingSenderId: '1084760992823',
-  appId: '1:1084760992823:web:c6402249f92d54372ce3b2'
+class Firebase {
+  constructor () {
+    this.config = {
+      apiKey: 'AIzaSyBdCSbXtHoTPO4JfPDicPhnams3q1p_6AQ',
+      authDomain: 'abdulla-2c3a5.firebaseapp.com',
+      databaseURL: 'https://abdulla-2c3a5.firebaseio.com',
+      projectId: 'abdulla-2c3a5',
+      storageBucket: 'abdulla-2c3a5.appspot.com',
+      messagingSenderId: '1084760992823',
+      appId: '1:1084760992823:web:c6402249f92d54372ce3b2'
+    }
+    fb.initializeApp(this.config)
+    this.db = fb.database()
+  }
 }
 
-firebase.initializeApp(firebaseConfig)
+module.exports.Firebase = Firebase
 
-firebase.database().ref('users').set('Its working!')
+var firebase = new Firebase()
+firebase.db.ref('users')
+  .once('value')
+  .then((snapshot) => {
+    console.log(snapshot.val())
+  })
+  .catch((e) => {
+    console.log(e.message)
+  })
