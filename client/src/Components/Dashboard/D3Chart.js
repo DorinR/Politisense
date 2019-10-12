6256e64675a0bb1183c81f887def48dac9ad7941
khalid baraka <baraka.khalid4@gmail.com>
2019-10-11 20:13:01 -0400

    refactored D3chart #9 #35

--- client/src/Components/Dashboard/D3Chart.js
+++ client/src/Components/Dashboard/D3Chart.js
@@ -1,119 +1,105 @@
 import * as d3 from 'd3'
 import React, { useRef, useEffect } from 'react'
-import {legend} from 'd3'
 
+export const D3Chart = (props) => {
+  var yesCounter = 0
+  var noCounter = 0
 
-const header_style = {
-    "width": 200,
-    "height":0
-}
+  const ref = useRef(null)
+  const billsData = props.data
 
- export const D3Chart = (props)=> {
+  billsData.map(d => {
+    console.log(d.FirstName)
+    if (d.FirstName.includes('Nay')) { noCounter++ }
+    if (d.FirstName.includes('Yea')) {
+      yesCounter++
+    }
+  })
 
-        var yes_counter=0
-        var no_counter= 0
+  const dataObjects = [{ index: 0, value: yesCounter, title: "Total Yes's" }, { index: 1, value: noCounter, title: "Total No's" }]
 
-        const ref = useRef(null);
+  const createPie = d3
+    .pie()
+    .value(d => d.value)
+    .sort(null)
 
-        const bills_data = props.data
+  const createArc = d3
+    .arc()
+    .innerRadius(props.innerRadius)
+    .outerRadius(props.outerRadius)
 
-        bills_data.map(d =>{
-            console.log(d.FirstName)
-             if(d.FirstName.includes("Nay") )
-                 {no_counter++}
-             if(d.FirstName.includes("Yea")){
-                 yes_counter++}
-            })
+  const colors = d3.scaleOrdinal(d3.schemeCategory10)
 
-        const data_objects= [{index:0, value: yes_counter,title: "Total Yes's"},{index:1,value:no_counter,title:"Total No's"}]
+  const format = d3.format('.2f')
 
-        const createPie = d3
-            .pie()
-            .value(d => d.value)
-            .sort(null);
+  useEffect(
+    () => {
+      const data = createPie(dataObjects)
 
-        const createArc = d3
-            .arc()
-            .innerRadius(props.innerRadius)
-            .outerRadius(props.outerRadius)
+      const group = d3.select(ref.current)
+      const groupWithData = group.selectAll('g.arc').data(data)
 
+      groupWithData.exit().remove()
 
-        const colors = d3.scaleOrdinal(d3.schemeCategory10);
+      const groupWithUpdate = groupWithData
+        .enter()
+        .append('g')
+        .attr('class', 'arc')
 
-        const format = d3.format(".2f");
+      const path = groupWithUpdate
+        .append('path')
+        .merge(groupWithData.select('path.arc'))
 
-        useEffect(
-            ()=> {
+      path
+        .attr('class', 'arc')
+        .attr('d', createArc)
+        .attr('fill', (d, i) => colors(i))
 
-            const data = createPie(data_objects);
+      const text = groupWithUpdate
+        .append('text')
+        .merge(groupWithData.select('text'))
 
-            const group = d3.select(ref.current)
-            const groupWithData = group.selectAll("g.arc").data(data)
+      text
+        .attr('text-anchor', 'middle')
+        .attr('alignment-baseline', 'middle')
+        .attr('transform', d => `translate(${createArc.centroid(d)})`)
+        .style('fill', 'white')
+        .style('font-size', 10)
+        .text(d => format(d.value))
 
+      var svg = d3.select('#my_data').append('g').attr('transform', 'translate(100 60)')
+      var keys = ['No', 'Yes']
 
-            groupWithData.exit().remove();
+      svg.selectAll('mydots')
+        .data(keys)
+        .enter()
+        .append('circle')
+        .attr('cx', 100)
+        .attr('cy', function (d, i) { return 100 + i * 25 })
+        .attr('r', 7)
+        .style('fill', function (d, i) { return colors(i) })
 
-            const groupWithUpdate = groupWithData
-                .enter()
-                .append("g")
-                .attr("class", "arc");
+      svg.selectAll('mylabels')
+        .data(keys)
+        .enter()
+        .append('text')
+        .attr('x', 120)
+        .attr('y', function (d, i) { return 100 + i * 25 })
+        .style('fill', function (d, i) { return colors(i) })
+        .text(function (d) { return d })
+        .attr('text-anchor', 'left')
+        .style('alignment-baseline', 'middle')
+    }
+    , [props.data])
 
-            const path = groupWithUpdate
-                .append("path")
-                .merge(groupWithData.select("path.arc"));
-
-            path
-                .attr("class", "arc")
-                .attr("d", createArc)
-                .attr("fill", (d, i) => colors(i));
-
-            const text = groupWithUpdate
-                .append("text")
-                .merge(groupWithData.select("text"));
-
-            text
-                .attr("text-anchor", "middle")
-                .attr("alignment-baseline", "middle")
-                .attr("transform", d => `translate(${createArc.centroid(d)})`)
-                .style("fill", "white")
-                .style("font-size", 10)
-                .text(d => format(d.value))
-
-            var svg = d3.select("#my_data").append("g").attr("transform","translate(100 60)")
-            var keys = ["No","Yes"]
-
-            svg.selectAll("mydots")
-                .data(keys)
-                .enter()
-                .append("circle")
-                .attr("cx", 100)
-                .attr("cy", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
-                .attr("r", 7)
-                .style("fill", function(d,i){ return colors(i)})
-
-            svg.selectAll("mylabels")
-                .data(keys)
-                .enter()
-                .append("text")
-                .attr("x", 120)
-                .attr("y", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
-                .style("fill", function(d,i){ return colors(i)})
-                .text(function(d){ return d})
-                .attr("text-anchor", "left")
-                .style("alignment-baseline", "middle")
-
-        }
-        , [props.data]);
-
-    return (
-        <div>
-             <svg id="my_data" width={props.width} height={props.height}>
-                <g
-                ref={ref}
-                transform={`translate(${props.height/2} ${props.height/2})`}
-                />
-            </svg>
-        </div>
-)
-
+  return (
+    <div>
+      <svg id='my_data' width={props.width} height={props.height}>
+        <g
+          ref={ref}
+          transform={`translate(${props.height / 2} ${props.height / 2})`}
+        />
+      </svg>
+    </div>
+  )
 }
