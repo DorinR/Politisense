5c1b3c8b64c0e2c30616a35db918a6cf02420187
Refat Kamal <refat.k17@gmail.com>
2019-11-01 17:39:58 -0400

    #12 set up xml parser for storing a dictionary of mps and their vote for each bill

---
+++ client/src/xmlDataParser/VoteParticipantsXmlParser.js
@@ -0,0 +1,45 @@
+import { XmlDataParser } from './XmlDataParser'
+
+class VoteParticipantsXmlParser extends XmlDataParser {
+  get TAG_NAME () {
+    return 'VoteParticipant'
+  }
+
+  get LIST_TAG_NAME () {
+    return 'List'
+  }
+
+  generateNewParser (xml) {
+    return new VoteParticipantsXmlParser(xml)
+  }
+
+  xmlToJson () {
+    const participant = {}
+
+    participant.name = this.getDataInTag('FirstName') + ' ' + this.getDataInTag('LastName')
+
+    // values can be 'Yea', 'Nay', or ''
+    const voteValue = this.getDataInTag('VoteValueName')
+    participant.vote = (voteValue === 'Paired') ? '' : voteValue
+
+    participant.paired = this.getDataInTag('Paired') === '1'
+  }
+
+  getAllFromXml () {
+    const participants = super.getAllFromXml()
+
+    // rather than array, use a key value JSON where the key is the Mps name
+    const votes = {}
+    for (const participant of participants) {
+      const name = participant.name
+      votes[name] = {
+        vote: participant.vote,
+        paired: participant.paired
+      }
+    }
+
+    return votes
+  }
+}
+
+module.exports.VoteParticipantsXmlParser = VoteParticipantsXmlParser
