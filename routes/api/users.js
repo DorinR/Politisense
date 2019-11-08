70308d82ae34b1b02469e9637f6d811df3b42761
khalid baraka <baraka.khalid4@gmail.com>
2019-11-07 20:11:29 -0500

    refactored users to allow axios calls in the backend  #8 #7

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,11 @@
 import express from 'express'
 const router = express.Router()
+const controller = require('../../controllers/Controller')
 
-// @route GET api/users
-// @desc Register User
-// @access Public
 router.get('/', (req, res) => res.send('user route'))
+router.post('/signup', controller.userSignup)
+router.post('/login', controller.userLogin)
+router.post('/setRiding', controller.setRiding)
+router.post('/check', controller.check)
 
 module.exports = router
