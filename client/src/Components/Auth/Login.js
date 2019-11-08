08ecbd632d131da6b4effe7ea886ac1e06e94bad
khalid baraka <baraka.khalid4@gmail.com>
2019-11-07 19:27:36 -0500

    integrated the social providers functionality #7 #8
    integrated the social providers functionality #7 #8

--- client/src/Components/Auth/Login.js
+++ client/src/Components/Auth/Login.js
@@ -10,7 +10,21 @@ import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
 import canadaimage from '../../assets/canada.jpg'
 import logo from '../../assets/PolotisenseTentativeLogo.png'
+import axios from 'axios'
+import { Firestore } from './../../Firebase'
 
+import {
+  FacebookLoginButton,
+  GoogleLoginButton,
+  TwitterLoginButton,
+  MicrosoftLoginButton
+
+} from 'react-social-login-buttons'
+
+const gridStyle = {
+  display: 'flex',
+  justifyContent: 'center'
+}
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -45,18 +59,49 @@ const useStyles = makeStyles(theme => ({
   },
   logo: {
     margin: theme.spacing(15, 0, 2)
+  },
+  quote: {
+    color: '#43D0C4'
   }
 }))
 
 export default function Login () {
   const classes = useStyles()
-
-  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [authenticated, setAuthenticated] = useState(false)
+  const [errors, setErrors] = useState({ email: '', password: '' })
+  const db = new Firestore()
+  const microsoftProvider = db.microsoftProvider
+  const twitterProvider = db.twitterProvider
+  const facebookProvider = db.facebookProvider
+  const googleProvider = db.googleProvider
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value)
+  function signInWithSocialProviders (_provider) {
+    return db.firebase.auth().signInWithPopup(_provider)
+  }
+
+  function validateUserFromSocialProviders (type, callback) {
+    const user = callback(type)
+        .then(user => {
+          axios.post('http://localhost:5000/api/users/check', { email: user.email }).then((res) => {
+            if (res.data.success) {
+              localStorage.setItem('user', JSON.stringify(user.email))
+              setAuthenticated(true)
+            } else {
+              const newUser = {
+                firstname: user.displayName.substr(0, user.displayName.indexOf(' ')),
+                lastname: user.displayName.substr(user.displayName.indexOf(' ') + 1),
+                email: user.email
+              }
+              props.history.push({
+                pathname: '/question',
+                state: { user: newUser }
+              })
+            }
+          })
+        })
+        .catch(e => { console.log(e) })
   }
 
   function handleSocialLogin (social) {
@@ -127,20 +172,22 @@ export default function Login () {
       setErrors(errors)
     }
   }
+
+  if (authenticated) {
+    return <Redirect to={{ pathname: '/dashboard' }} />
+  }
+  
   return (
-    authenticated
-      ? (<Redirect to='/dashboard' />)
-      : (
-        <Grid container component='main' className={classes.root}>
-          <CssBaseline />
-          <Grid item xs={false} sm={4} md={7} className={classes.image} />
-          <Grid item xs={12} sm={8} md={5}>
-            <Grid container>
-              <Grid item sm={12}>
-                <div className={classes.paper}>
-                  <img src={logo} alt='' className={classes.logo} />
-                  <form className={classes.form} noValidate onSubmit={handleSubmit}>
-                    <TextField
+      <Grid container component='main' className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5}>
+          <Grid container>
+            <Grid item sm={12}>
+              <div className={classes.paper}>
+                <img src={logo} alt='' className={classes.logo} />
+                <form className={classes.form} noValidate onSubmit={handleSubmit}>
+                  <TextField
                       variant='outlined'
                       margin='normal'
                       required
@@ -149,11 +196,13 @@ export default function Login () {
                       label='Email Address'
                       name='email'
                       autoComplete='email'
-                      onChange={handleUsernameChange}
-                      value={username}
+                      onChange={handleEmailChange}
+                      value={email}
+                      error={errors.email !== ''}
+                      helperText={errors.email}
                       autoFocus
-                    />
-                    <TextField
+                  />
+                  <TextField
                       variant='outlined'
                       margin='normal'
                       required
@@ -165,52 +214,59 @@ export default function Login () {
                       autoComplete='current-password'
                       onChange={handlePasswordChange}
                       value={password}
-                    />
-                    <Typography variant='h6' gutterBottom style={{ textAlign: 'center' }}>
-                      OR
-                    </Typography>
-                    <Grid
-                      container
-                    >
-                      <Grid item xs={3} className={classes.social}>
-                        <FontAwesomeIcon icon={faFacebook} size='4x' color='#455892' />
-                      </Grid>
-                      <Grid item xs={3} className={classes.social}>
-                        <FontAwesomeIcon icon={faTwitter} size='4x' color='#55ADEC' />
-                      </Grid>
-                      <Grid item xs={3} className={classes.social}>
-                        <FontAwesomeIcon icon={faLinkedin} size='4x' color='#0274B3' />
-                      </Grid>
-                      <Grid item xs={3} className={classes.social}>
-                        <FontAwesomeIcon icon={faGoogle} size='4x' color='#E43E2B' />
-                      </Grid>
-                    </Grid>
-                    <Button
+                      error={errors.password !== ''}
+                      helperText={errors.password}
+                  />
+                  <Button
                       type='submit'
                       fullWidth
                       variant='contained'
                       color='primary'
                       className={classes.submit}
-                    >
-                      Log in
-                    </Button>
+                  >
+                    Log in
+                  </Button>
+                  <Grid container>
+                    <Grid item xs>
+                      <Link variant='body2' to='/signup' className={classes.routerLink}>
+                        Forgot password?
+                      </Link>
+                    </Grid>
+                    <Grid item>
+                      <Link variant='body2' to='/signup' className={classes.routerLink}>
+                        {"Don't have an account? Sign Up"}
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </form>
+                <Typography variant='h6' gutterBottom style={{ textAlign: 'center' }}>
+                  OR
+                </Typography>
+                <div className='Wrapper' style={gridStyle}>
+                  <Grid container>
                     <Grid container>
-                      <Grid item xs>
-                        <Link variant='body2' to='/signup' className={classes.routerLink}>
-                          Forgot password?
-                        </Link>
+                      <Grid item xs={6} className={classes.social}>
+                        <FacebookLoginButton onClick={() => validateUserFromSocialProviders('facebook', handleSocialLogin)} />
                       </Grid>
-                      <Grid item>
-                        <Link variant='body2' to='/signup' className={classes.routerLink}>
-                          {"Don't have an account? Sign Up"}
-                        </Link>
+                      <Grid item xs={6} className={classes.social}>
+                        <TwitterLoginButton onClick={() => validateUserFromSocialProviders('twitter', handleSocialLogin)} />
                       </Grid>
                     </Grid>
-                  </form>
+                    <Grid container>
+                      <Grid item xs={6} className={classes.social}>
+                        <GoogleLoginButton type='button' id='test' onClick={() => validateUserFromSocialProviders('google', handleSocialLogin)} />
+                      </Grid>
+                      <Grid item xs={6} className={classes.social}>
+                        <MicrosoftLoginButton onClick={() => validateUserFromSocialProviders('microsoft', handleSocialLogin)} />
+                      </Grid>
+                    </Grid>
+                  </Grid>
                 </div>
-              </Grid>
+              </div>
             </Grid>
           </Grid>
-        </Grid>)
-  )
+        </Grid>
+      </Grid>)
+
+
 }
