2b137bfcc8223491904e3ae1bfdee3dda2f05bac
mathewplouffe <m_plo@encs.concordia.ca>
2019-11-08 18:31:32 -0500

    #61 [spike/data-model] : modified VoteRecord Model

--- models/VoteRecord.js
+++ models/VoteRecord.js
@@ -1,9 +1,11 @@
 const Condition = require('./Condition').Condition
 
 class VoteRecord {
-  constructor (billNumber, id, name, voters) {
+  constructor (billNumber, id, name, voters, yeas, nays) {
     Condition.parameter(billNumber).isType(String)
     Condition.parameter(id).isType(Number)
+    Condition.parameter(yeas).isType(Number)
+    Condition.parameter(nays).isType(Number)
     Condition.parameter(name).isType(String)
     Condition.parameter(voters).isType(Object)
 
@@ -11,6 +13,9 @@ class VoteRecord {
     this.id = id
     this.name = name
     this.voters = voters
+    this.yeas = yeas
+    this.nays = nays
+    this.assent = Boolean(yeas > nays)
   }
 }
 
