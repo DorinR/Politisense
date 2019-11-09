9f395c773caa3f5a1f0c1a50d60c66053a555a38
mathewplouffe <m_plo@encs.concordia.ca>
2019-11-09 12:07:37 -0500

    #63 [feature/tdg-impl] : incorporated review comments
    #63 [feature/tdg-impl] : incorporated review comments
    
    #63 [feature/tdg-impl] : incorporated review comments

--- client/src/test/firebase.test.js
+++ client/src/test/firebase.test.js
@@ -4,45 +4,46 @@ const chai = require('chai')
 chai.should()
 const chaiPromise = require('chai-as-promised')
 chai.use(chaiPromise)
-// eslint-disable-next-line no-undef
+/* eslint-env jest */
 describe('All firebase tests', () => {
-  // eslint-disable-next-line no-undef
+  let fb
+  beforeEach(() => {
+    fb = new Firestore()
+  })
+
+  afterEach(async () => {
+    await fb.close()
+  })
+
   test('Can insert and delete a record', () => {
-    const fb = new Firestore()
     fb.Bill().insert({ id: 'a' }).should.eventually.be.a(true)
-    fb.Bill().delete({ id: 'a' }).should.eventually.be.a(1)
-    fb.close()
+    fb.Bill()
+      .where('id', '==', 'a')
+      .delete().should.eventually.be.a(1)
   })
-  // eslint-disable-next-line no-undef
+
   test('can retrieve records', () => {
-    const fb = new Firestore()
     fb.Bill().select().should.eventually.be.a('object')
-    fb.close()
   })
-  // eslint-disable-next-line no-undef
+
   test('can filter records (legacy)', () => {
-    const fb = new Firestore()
     fb.Bill().select('id', '==', '2').should.eventually.be.a('object')
-    fb.close()
   })
-  // eslint-disable-next-line no-undef
+
   test('can filter records (use this)', () => {
-    const fb = new Firestore()
     fb.Bill()
       .where('id', '==', '2')
       .select().should.eventually.be.a('object')
-    fb.close()
   })
-  // eslint-disable-next-line no-undef
+
   test('can update a record', () => {
-    const fb = new Firestore()
     fb.Bill().insert({ id: '2' }).should.eventually.be.a(true)
+    fb.Bill().insert({ id: '3' }).should.eventually.be.a(true)
     fb.Bill()
       .where('id', '==', '2')
       .update({ id: '3' }).should.eventually.be.a(1)
     fb.Bill()
       .where('id', '==', '3')
-      .delete().should.eventually.be.a(1)
-    fb.close()
+      .delete().should.eventually.be.a(2)
   })
 })
