01dec62c11c2fbab1dfa3d9ed014ef87ab56ab1f
TheAlexRobert <alexandrerobertsmeets@gmail.com>
2019-11-09 19:48:35 -0500

    #7 #8 code cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,63 @@
-import React from "react";
+import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch
-} from "react-router-dom";
-import Login from "./Components/Auth/Login";
-import SignUp from "./Components/Auth/SignUp";
-import Navbar from "./Components/Navbar";
-import Dashboard from "./Components/Dashboard/Dashboard";
-import Logout from "./Components/Logout";
-import UserAccount from "./Components/UserAccount";
-import Map from "./Components/Map";
-import Questionnaire from "./Components/Questionnaire";
+} from 'react-router-dom'
+import Login from './Components/Auth/Login'
+import SignUp from './Components/Auth/SignUp'
+import Navbar from './Components/Navbar'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Logout from './Components/Logout'
+import UserAccount from './Components/UserAccount'
+import Map from './Components/Map'
+import Questionnaire from './Components/Questionnaire'
 
 const App = () => {
   const LoginContainer = () => (
-    <div className="container">
-      <Route exact path="/" render={() => <Redirect to="/login" />} />
-      <Route path="/signup" component={SignUp} />
-      <Route path="/login" component={Login} />
+    <div className='container'>
+      <Route exact path='/' render={() => <Redirect to='/login' />} />
+      <Route path='/signup' component={SignUp} />
+      <Route path='/login' component={Login} />
     </div>
-  );
+  )
   const DefaultContainer = () => (
     <div>
       <Navbar>
         <div>
-          <Route exact path="/" render={() => <Redirect to="/login" />} />
-          <PrivateRoute path="/dashboard" component={Dashboard} />
-          <PrivateRoute path="/logout" component={Logout} />
-          <PrivateRoute path="/map" component={Map} />
-          <PrivateRoute path="/account" component={UserAccount} />
+          <Route exact path='/' render={() => <Redirect to='/login' />} />
+          <PrivateRoute path='/dashboard' component={Dashboard} />
+          <PrivateRoute path='/logout' component={Logout} />
+          <PrivateRoute path='/map' component={Map} />
+          <PrivateRoute path='/account' component={UserAccount} />
         </div>
       </Navbar>
     </div>
-  );
+  )
 
   const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route
       {...rest}
       render={props =>
-        localStorage.getItem("user") ? (
+        localStorage.getItem('user') ? (
           <Component {...props} />
         ) : (
-          <Redirect to="/login" />
-        )
-      }
+          <Redirect to='/login' />
+        )}
     />
-  );
+  )
 
   return (
     <Router>
       <Switch>
-        <Route exact path="/(login)" component={LoginContainer} />
-        <Route exact path="/signup" component={LoginContainer} />
-        <Route exact path="/question" component={Questionnaire} />
+        <Route exact path='/(login)' component={LoginContainer} />
+        <Route exact path='/signup' component={LoginContainer} />
+        <Route exact path='/question' component={Questionnaire} />
         <Route component={DefaultContainer} />
       </Switch>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- client/src/Components/Questionnaire.js
+++ client/src/Components/Questionnaire.js
@@ -1,32 +1,32 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Stepper from "@material-ui/core/Stepper";
-import Step from "@material-ui/core/Step";
-import StepLabel from "@material-ui/core/StepLabel";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import AppBar from "@material-ui/core/AppBar/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Grid from "@material-ui/core/Grid";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormControl from "@material-ui/core/FormControl";
-import FormLabel from "@material-ui/core/FormLabel";
-import TextField from "@material-ui/core/TextField";
-import axios from "axios";
-import { Redirect } from "react-router";
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Stepper from '@material-ui/core/Stepper'
+import Step from '@material-ui/core/Step'
+import StepLabel from '@material-ui/core/StepLabel'
+import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import AppBar from '@material-ui/core/AppBar/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Grid from '@material-ui/core/Grid'
+import Radio from '@material-ui/core/Radio'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import FormControl from '@material-ui/core/FormControl'
+import FormLabel from '@material-ui/core/FormLabel'
+import TextField from '@material-ui/core/TextField'
+import axios from 'axios'
+import { Redirect } from 'react-router'
 
 const useStyles = makeStyles(theme => ({
-  "@global": {
+  '@global': {
     body: {
       backgroundColor: theme.palette.common.white
     }
   },
   root: {
-    width: "100%"
+    width: '100%'
   },
   button: {
     marginRight: theme.spacing(1)
@@ -39,13 +39,13 @@ const useStyles = makeStyles(theme => ({
     minWidth: 275
   },
   bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
   },
   title: {
     flexGrow: 1,
-    textAlign: "center"
+    textAlign: 'center'
   },
   pos: {
     marginBottom: 12
@@ -54,95 +54,95 @@ const useStyles = makeStyles(theme => ({
     marginTop: 10
   },
   test: {
-    textAlign: "center"
+    textAlign: 'center'
   }
-}));
+}))
 
-function getSteps() {
-  return ["Voting Issue", "Area of Interest", "Postal Code"];
+function getSteps () {
+  return ['Voting Issue', 'Area of Interest', 'Postal Code']
 }
 
-export async function setRiding(postalCode) {
-  let result = "";
+export async function setRiding (postalCode) {
+  let result = ''
   await axios
-    .post("http://localhost:5000/api/users/setRiding", {
+    .post('http://localhost:5000/api/users/setRiding', {
       postalCode: postalCode
     })
     .then(res => {
-      result = res;
-    });
-  return result;
+      result = res
+    })
+  return result
 }
 
-export default function HorizontalLinearStepper(props) {
-  const classes = useStyles();
-  const [activeStep, setActiveStep] = useState(0);
-  const steps = getSteps();
+export default function HorizontalLinearStepper (props) {
+  const classes = useStyles()
+  const [activeStep, setActiveStep] = useState(0)
+  const steps = getSteps()
   const [options, setOptions] = useState([
-    "Economics",
-    "Social Issues",
-    "Healthcare"
-  ]);
-  const [category1, setCatergory1] = useState("Economics");
-  const [category2, setCatergory2] = useState("");
-  const [errors, setErrors] = useState({ postalCode: "" });
-  const [postalCode, setPostalCode] = useState("");
+    'Economics',
+    'Social Issues',
+    'Healthcare'
+  ])
+  const [category1, setCatergory1] = useState('Economics')
+  const [category2, setCatergory2] = useState('')
+  const [errors, setErrors] = useState({ postalCode: '' })
+  const [postalCode, setPostalCode] = useState('')
 
   const handleChangeCategory1 = event => {
-    setCatergory1(event.target.value);
-  };
+    setCatergory1(event.target.value)
+  }
 
   const handleChangeCategory2 = event => {
-    setCatergory2(event.target.value);
-  };
+    setCatergory2(event.target.value)
+  }
 
-  function getStepContent(step) {
+  function getStepContent (step) {
     switch (step) {
       case 0:
         return (
           <Card className={classes.card}>
             <CardContent>
-              <FormControl component="fieldset" className={classes.formControl}>
-                <FormLabel component="legend">
+              <FormControl component='fieldset' className={classes.formControl}>
+                <FormLabel component='legend'>
                   What issue brings you out to vote?
                 </FormLabel>
                 <RadioGroup
-                  aria-label="issue"
-                  name="issue"
+                  aria-label='issue'
+                  name='issue'
                   value={category1}
                   onChange={handleChangeCategory1}
                 >
                   <FormControlLabel
-                    value="Economics"
+                    value='Economics'
                     control={<Radio />}
-                    label="Economics"
+                    label='Economics'
                   />
                   <FormControlLabel
-                    value="Social Issues"
+                    value='Social Issues'
                     control={<Radio />}
-                    label="Social Issues"
+                    label='Social Issues'
                   />
                   <FormControlLabel
-                    value="Healthcare"
+                    value='Healthcare'
                     control={<Radio />}
-                    label="Healthcare"
+                    label='Healthcare'
                   />
                 </RadioGroup>
               </FormControl>
             </CardContent>
           </Card>
-        );
+        )
       case 1:
         return (
           <Card className={classes.card}>
             <CardContent>
-              <FormControl component="fieldset" className={classes.formControl}>
-                <FormLabel component="legend">
+              <FormControl component='fieldset' className={classes.formControl}>
+                <FormLabel component='legend'>
                   What issue would you like to know more about?
                 </FormLabel>
                 <RadioGroup
-                  aria-label="issue"
-                  name="issue"
+                  aria-label='issue'
+                  name='issue'
                   value={category2}
                   onChange={handleChangeCategory2}
                 >
@@ -161,98 +161,98 @@ export default function HorizontalLinearStepper(props) {
               </FormControl>
             </CardContent>
           </Card>
-        );
+        )
       case 2:
         return (
           <form noValidate>
             <TextField
               onChange={e => setPostalCode(e.target.value)}
-              variant="outlined"
+              variant='outlined'
               required
               fullWidth
-              id="postal_code"
-              label="Postal Code"
-              name="postalCode"
-              error={errors.postalCode !== ""}
+              id='postal_code'
+              label='Postal Code'
+              name='postalCode'
+              error={errors.postalCode !== ''}
               helperText={errors.postalCode}
             />
           </form>
-        );
+        )
       default:
-        return "Unknown step";
+        return 'Unknown step'
     }
   }
 
   const handleSubmit = e => {
-    e.preventDefault();
+    e.preventDefault()
     if (postalCode) {
       setRiding(postalCode)
         .then(res => {
           if (res.data.success) {
-            const userToSignup = props.location.state.user;
-            userToSignup.postalCode = postalCode;
-            userToSignup.riding = res.data.data;
-            userToSignup.category1 = category1;
-            userToSignup.category2 = category2;
-            localStorage.setItem("user", JSON.stringify(userToSignup));
-            axios.post("http://localhost:5000/api/users/signup", userToSignup);
-            props.history.push("/dashboard");
+            const userToSignup = props.location.state.user
+            userToSignup.postalCode = postalCode
+            userToSignup.riding = res.data.data
+            userToSignup.category1 = category1
+            userToSignup.category2 = category2
+            localStorage.setItem('user', JSON.stringify(userToSignup))
+            axios.post('http://localhost:5000/api/users/signup', userToSignup)
+            props.history.push('/dashboard')
           } else {
-            console.log("Could not fetch riding");
+            console.log('Could not fetch riding')
           }
         })
         .catch(error => {
-          console.log(error);
-        });
+          console.log(error)
+        })
     }
-  };
+  }
 
   const checkValidPostalCode = () => {
-    const postalCodeFormat = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/;
-    const errors = {};
+    const postalCodeFormat = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/
+    const errors = {}
     errors.postalCode = !postalCode.match(postalCodeFormat)
-      ? "Invalid postal code"
-      : "";
-    setErrors(errors);
-    return errors.postalCode === "";
-  };
+      ? 'Invalid postal code'
+      : ''
+    setErrors(errors)
+    return errors.postalCode === ''
+  }
 
   const handleNext = () => {
-    let valid = true;
+    let valid = true
     if (activeStep === steps.length - 2) {
-      valid = category2;
+      valid = category2
     }
     if (activeStep === steps.length - 1) {
-      valid = checkValidPostalCode();
+      valid = checkValidPostalCode()
     }
     if (valid) {
-      setActiveStep(prevActiveStep => prevActiveStep + 1);
+      setActiveStep(prevActiveStep => prevActiveStep + 1)
     }
-  };
+  }
 
   const handleBack = () => {
     if (activeStep === steps.length - 2) {
-      setCatergory2("");
+      setCatergory2('')
     }
-    setActiveStep(prevActiveStep => prevActiveStep - 1);
-  };
+    setActiveStep(prevActiveStep => prevActiveStep - 1)
+  }
 
   const handleReset = () => {
-    setActiveStep(0);
-    setPostalCode("");
-  };
+    setActiveStep(0)
+    setPostalCode('')
+  }
 
   if (props.location.state === undefined) {
-    return <Redirect to="/login" />;
+    return <Redirect to='/login' />
   }
 
   return (
     <div className={classes.root}>
       <div>
         <div>
-          <AppBar position="static">
+          <AppBar position='static'>
             <Toolbar>
-              <Typography variant="h6" className={classes.title}>
+              <Typography variant='h6' className={classes.title}>
                 Questionnaire
               </Typography>
             </Toolbar>
@@ -260,22 +260,22 @@ export default function HorizontalLinearStepper(props) {
         </div>
       </div>
       <div className={classes.pad}>
-        <Grid container direction="row" justify="center" alignItems="center">
+        <Grid container direction='row' justify='center' alignItems='center'>
           <Grid item xs={8}>
             <Stepper activeStep={activeStep}>
               {steps.map((label, index) => {
-                const stepProps = {};
-                const labelProps = {};
+                const stepProps = {}
+                const labelProps = {}
                 return (
                   <Step key={label} {...stepProps}>
                     <StepLabel {...labelProps}>{label}</StepLabel>
                   </Step>
-                );
+                )
               })}
             </Stepper>
           </Grid>
         </Grid>
-        <Grid container direction="row" justify="center" alignItems="center">
+        <Grid container direction='row' justify='center' alignItems='center'>
           <Grid item xs={3}>
             <div className={classes.test}>
               {activeStep === steps.length ? (
@@ -286,8 +286,8 @@ export default function HorizontalLinearStepper(props) {
                   <Button onClick={handleReset}>Reset</Button>
                   <Button
                     onClick={handleSubmit}
-                    variant="contained"
-                    color="primary"
+                    variant='contained'
+                    color='primary'
                     className={classes.button}
                   >
                     Confirm information
@@ -309,8 +309,8 @@ export default function HorizontalLinearStepper(props) {
                       Back
                     </Button>
                     <Button
-                      variant="contained"
-                      color="primary"
+                      variant='contained'
+                      color='primary'
                       onClick={handleNext}
                       className={classes.button}
                     >
@@ -324,5 +324,5 @@ export default function HorizontalLinearStepper(props) {
         </Grid>
       </div>
     </div>
-  );
+  )
 }
