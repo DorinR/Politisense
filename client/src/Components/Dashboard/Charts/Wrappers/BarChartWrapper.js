8f5fd7f9584b5f95c21e56679c73e0140d4a3f2e
khalid baraka <baraka.khalid4@gmail.com>
2019-11-11 03:56:11 -0500

    #9 #35 integrating the d3 charts into the dahsbaord

---
+++ client/src/Components/Dashboard/Charts/Wrappers/BarChartWrapper.js
@@ -0,0 +1,25 @@
+import React,{Component} from "react"
+import BarPieChart from "../BarPieCharts";
+import BarChart from '../BarChart'
+import DonutChart from "../DonutChart";
+// import {D3Chart} from '../D3Chart'
+export default class BarChartWrapper extends Component{
+    componentDidMount() {
+        switch (this.props.type){
+            case "bar-pie":
+                return new BarPieChart(this.refs.chart)
+                break
+            case "bar":
+                return new BarChart(this.refs.chart)
+                break
+            default: return  new DonutChart(this.refs.chart)
+        }
+    }
+    render() {
+        return (
+            <div>
+                <div ref="chart" ></div>
+            </div>
+        )
+    }
+}
