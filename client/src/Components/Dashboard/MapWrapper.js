129d3d0ecc98f8bd862e5b3cc49f114768b4b59f
Ishmam Murtaza <ishmammurtaza@gmail.com>
2019-11-10 19:00:58 -0500

    #24 fixed formatting

--- client/src/Components/Dashboard/MapWrapper.js
+++ client/src/Components/Dashboard/MapWrapper.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react'
 import CanadaMap from './CanadaMap'
 
 const divStyle = {
-    marginTop: 70,
-    marginLeft: 100
+  marginTop: 70,
+  marginLeft: 100
 }
 const headerStyle = {
-    marginLeft: 450,
-    fontSize: 40
+  marginLeft: 450,
+  fontSize: 40
 }
 export default class ChartWrapper extends Component {
-    componentDidMount () {
-        new CanadaMap(this.refs.chart)
-    }
+  componentDidMount () {
+    new CanadaMap(this.refs.chart)
+  }
 
-    render () {
-        return (
-            <div>
-                <h1 style={headerStyle}> Map of Canada </h1>
-                <div style={divStyle} ref='chart' />
-            </div>)
-    }
+  render () {
+    return (
+      <div>
+        <h1 style={headerStyle}> Map of Canada </h1>
+        <div style={divStyle} ref='chart' />
+      </div>)
+  }
 }
