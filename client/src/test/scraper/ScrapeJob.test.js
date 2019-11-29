d767a1d22f7657d185025645f3bfbc217a6f4b0e
mathewplouffe <m_plo@encs.concordia.ca>
2019-11-29 17:43:21 -0500

    #86 [bugfix/tests] : refactored Scrape Job tests

--- client/src/test/scraper/ScrapeJob.test.js
+++ client/src/test/scraper/ScrapeJob.test.js
@@ -1,17 +1,62 @@
+/* eslint-env jest */
+
 import { ScrapeJob } from '../../scraper/ScrapeJob'
 import { ScrapeRunner } from '../../scraper/ScrapeRunner'
 const chai = require('chai')
-chai.should()
-const chaiPromise = require('chai-as-promised')
-chai.use(chaiPromise)
-// eslint-disable-next-line no-undef
+const Assert = chai.assert
 describe('All Job tests', () => {
-  // eslint-disable-next-line no-undef
-  test('Job throws on bad link', () => {
+
+  let mockFn
+  let manager
+  beforeAll(() => {
+    mockFn = async (options) => {
+      if(options.uri === 'https://www.google.ca/') {
+        return {body: '<!DOCTYPE html>' +
+            '<html>' +
+            '<head>' +
+            '</head>' +
+            '<body>' +
+            '<a href=\"https://www.google.ca\">google</a>' +
+            '</body>' +
+            '<\html>'}
+      }
+      else {
+        throw new Error()
+      }
+    }
+    manager = new ScrapeRunner()
+    manager.enqueueJobsCb = () => {}
+  })
+
+  test('Job succeeds on good link', async (done) => {
+    const url = 'https://www.google.ca/'
+    const exc = new ScrapeJob(url, manager, [])
+    exc.scraper.send = mockFn
+    const didScrape = await exc.execute()
+      .then(res => {
+        return true
+      })
+      .catch(e => {
+        console.log(e)
+        return false
+      })
+    Assert.equal(didScrape, true)
+    done()
+  }, 10000)
+
+  test('Job throws on bad link', async (done) => {
     const url = 'https://'
-    const mngr = new ScrapeRunner()
-    mngr.enqueueJobsCb = () => {}
-    const exc = new ScrapeJob(url, mngr, [])
-    exc.execute().should.eventually.throw().notify(chai.done)
+    const exc = new ScrapeJob(url, manager, [])
+    exc.scraper.send = mockFn
+    const didScrape = await exc.execute()
+      .then(res => {
+        console.log(res)
+        return true
+      })
+      .catch(e => {
+        return false
+      })
+    Assert.equal(didScrape, false)
+    done()
   }, 10000)
 })
