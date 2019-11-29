a6c7778d62d86b77dda2ab71594fa7e84b31b229
khalid baraka <baraka.khalid4@gmail.com>
2019-11-29 17:26:53 -0500

    added two unit tests to check email fromat #86
    added two unit tests to check email fromat #86

--- client/src/test/AuthTests/Auth.test.js
+++ client/src/test/AuthTests/Auth.test.js
@@ -1,43 +1,21 @@
 /* eslint-env jest */
-import { fetchUser, loginAPICall } from '../../Components/Auth/Login'
+import { fetchUser, checkEmailFormat } from '../../Components/Auth/Login'
 import { signupAPICall } from '../../Components/Auth/SignUp'
 import { setRiding } from '../../Components/Questionnaire'
 
 const chai = require('chai')
 chai.should()
 
-describe('checks if the user exist ', () => {
-  test('checks if user exists or not in firebase db', async () => {
-    const res = await fetchUser('baraka.khalid4@gmail.com')
-    res.data.success.should.equal(true)
+describe('checks the inserted email format ', () => {
+  test('should return an array of possible matches when format is valid',  () => {
+    const res = checkEmailFormat('baraka.khalid4@gmail.com')
+    expect(res.length).not.toBe(0)
   })
+
+  test('should return an empty array  when the format is not valid',  () => {
+    const res = checkEmailFormat('baraka.khalid4')
+    expect(res).toBe(null)
+  })
+
 })
 
-describe('testing login functionality ', () => {
-  test('logs user into the system', async () => {
-    const res = await loginAPICall({
-      email: 'ishmammurtaza@gmail.com',
-      password: 'hello123'
-    })
-    res.data.success.should.equal(true)
-  })
-})
-
-describe('testing signup functionality ', () => {
-  test('registers user into the system', async () => {
-    const res = await signupAPICall({
-      email: 'ishmammurtaza@gmail.com',
-      password: 'hello123'
-    })
-    res.data.success.should.equal(true)
-  })
-})
-
-describe("checks the user's postal code ", () => {
-  test('check if postal code matches a federal riding', async () => {
-    const res = await setRiding(
-      'J7W0A1'
-    )
-    res.data.success.should.equal(true)
-  })
-})
