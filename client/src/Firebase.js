7dfd20a06be31bbda627808f2d94bf19a2817a88
DorinR <dorin.rogov@gmail.com>
2020-01-06 19:55:30 -0500

    #127 fixed minor error introduced when merging development into branch

--- client/src/Firebase.js
+++ client/src/Firebase.js
@@ -4,7 +4,7 @@ const fs = require('firebase')
 require('firebase/firestore')
 
 class _Firestore {
-  constructor () {
+  constructor() {
     this.config = {
       apiKey: 'AIzaSyBdCSbXtHoTPO4JfPDicPhnams3q1p_6AQ',
       authDomain: 'abdulla-2c3a5.firebaseapp.com',
@@ -29,7 +29,7 @@ class _Firestore {
 }
 
 var instance = null
-function getInstance () {
+function getInstance() {
   if (!instance) {
     instance = new _Firestore()
   }
@@ -37,13 +37,13 @@ function getInstance () {
 }
 
 class Reference {
-  constructor (reference) {
+  constructor(reference) {
     this.reference = reference
     this.modelsOnly = false
     this.query = null
   }
 
-  where (attribute, operator, value) {
+  where(attribute, operator, value) {
     if (!this.query) {
       this.query = this.reference.where(attribute, operator, value)
     }
@@ -51,7 +51,7 @@ class Reference {
     return this
   }
 
-  update (model) {
+  update(model) {
     if (this.modelsOnly && typeof model !== typeof new Model()) {
       throw new Error('Error: Only a model can be updated in firebase')
     }
@@ -85,13 +85,14 @@ class Reference {
     })
   }
 
-  delete () {
+  delete() {
     let ref = this.reference
     if (this.query) {
       ref = this.query
     }
     return new Promise((resolve, reject) => {
-      ref.get()
+      ref
+        .get()
         .then(async snapshot => {
           let count = 0
           const snapshotArray = []
@@ -100,11 +101,11 @@ class Reference {
           })
           await Promise.all(
             snapshotArray.map(ref => {
-              return ref.delete()
-                .then(resp => {
-                  count++
-                })
-            }))
+              return ref.delete().then(resp => {
+                count++
+              })
+            })
+          )
           resolve(count)
         })
         .catch(e => {
@@ -113,12 +114,14 @@ class Reference {
     })
   }
 
-  select (attribute, operator, value) {
+  select(attribute, operator, value) {
     let ref = this.reference.get.bind(this.reference)
-    if (typeof attribute !== 'undefined' &&
-        typeof operator !== 'undefined' &&
-        typeof value !== 'undefined' &&
-        this.query === null) {
+    if (
+      typeof attribute !== 'undefined' &&
+      typeof operator !== 'undefined' &&
+      typeof value !== 'undefined' &&
+      this.query === null
+    ) {
       const query = this.reference.where(attribute, operator, value)
       ref = query.get.bind(query)
     } else if (this.query !== null) {
@@ -135,7 +138,7 @@ class Reference {
     })
   }
 
-  insert (model) {
+  insert(model) {
     if (this.modelsOnly && typeof model !== typeof new Model()) {
       throw new Error('Error: Only a model can be inserted in firebase')
     }
@@ -156,7 +159,7 @@ class Reference {
 }
 
 class Firestore {
-  constructor () {
+  constructor() {
     this.firestore = getInstance()
     this.reference = this.firestore.db
     this.googleProvider = this.firestore.googleProvider
@@ -166,38 +169,39 @@ class Firestore {
     this.microsoftProvider = this.firestore.microsoftProvider
   }
 
-  Bill () {
+  Bill() {
     return new Reference(this.reference.collection('bills'))
   }
 
-  BillClassification () {
+  BillClassification() {
     return new Reference(this.reference.collection('bill_classification'))
   }
 
-  TfIdfClassification () {
+  TfIdfClassification() {
     return new Reference(this.reference.collection('tf_idf_bill'))
   }
 
-  Politician () {
+  Politician() {
     return new Reference(this.reference.collection('politicians'))
   }
 
-  User () {
+  User() {
     return new Reference(this.reference.collection('users'))
   }
 
-  VoteRecord () {
+  VoteRecord() {
     return new Reference(this.reference.collection('voteRecord'))
   }
 
-  Ridings () {
+  Ridings() {
     return new Reference(this.reference.collection('ridings'))
+  }
 
-  FinancialRecord () {
+  FinancialRecord() {
     return new Reference(this.reference.collection('financialRecord'))
   }
 
-  async close () {
+  async close() {
     await this.firestore.app
       .delete()
       .then(result => {
