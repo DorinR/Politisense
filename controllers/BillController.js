0ef5ffabd314f7e2e369a79f3b7262ee8a0bc0e0
Ishmam Murtaza <ishmammurtaza@gmail.com>
2020-01-15 22:45:33 -0500

    query test

--- controllers/BillController.js
+++ controllers/BillController.js
@@ -38,60 +38,63 @@ exports.getBillsByCategoryForRep = async (req, res) => {
   const db = new Firestore()
   let representative = req.body.representative
   let cateogry  = req.body.category
-  let bills = db.Bill()
-  let votes = db.Vote()
-  let voteRecords = db.VoteRecord()
-  let records = await votes.innerJoin('vote',voteRecords,'_id').catch(e=>console.log(e))
-  res.json({
-    success: true,
-    data: records
-  })
-
-  //let records2 = await records.innerJoin('bill',bills,'_id')
-  //let repId = ""
-  // db.Politician()
-  //     .select('name', '==', representative)
-  //     .then(snapshot => {
-  //       if (snapshot.empty) {
-  //         res.status(400).json({
-  //           message: 'Riding Not Found',
-  //           success: false
-  //         })
-  //       }
-  //       snapshot.forEach(doc => {
-  //         repId = doc.id
-  //       })
-  //     })
-
-
-
-
-  // db.Politician()
-  //     .select('name', '==', representative)
-  //     .then(snapshot => {
-  //       if (snapshot.empty) {
-  //         res.status(400).json({
-  //           message: 'Rep Not Found',
-  //           success: false
-  //         })
-  //       }
-  //       snapshot.forEach(doc => {
-  //         repId = doc.id
-  //       })
-  //       db.Vote()
-  //           .select('member', '==', repId)
-  //           .then(snapshot => {
-  //             if (snapshot.empty) {
-  //               res.status(400).json({
-  //                 message: 'Riding Not Found',
-  //                 success: false
-  //               })
-  //             }
-  //             snapshot.forEach(doc => {
-  //               voteEntries.push(doc.data())
-  //             })
-  //             let bills = db.Bill()
-  //             let records =  bills.innerJoin('_id',)
-  //           })
-  //     })
+  let voteEntries = []
+  let voteRecordEntries = []
+  let repId = ""
+  db.Politician()
+      .select('name', '==', representative)
+      .then(snapshot => {
+        if (snapshot.empty) {
+          res.status(400).json({
+            message: 'Rep Not Found',
+            success: false
+          })
+        }
+        snapshot.forEach(doc => {
+          repId = doc.id
+        })
+        db.Vote()
+            .select('member', '==', repId)
+            .then(snapshot => {
+              if (snapshot.empty) {
+                res.status(400).json({
+                  message: 'Riding Not Found',
+                  success: false
+                })
+              }
+              snapshot.forEach(doc => {
+                voteEntries.push(doc.data())
+              })
+              db.VoteRecord().select().then(snapshot => {
+                snapshot.forEach(doc => {
+                  let temp = {docId: doc.id, data: doc.data()}
+                  voteRecordEntries.push(temp)
+                })
+                for(let i = 0 ; i < voteEntries.length;i++){
+                  for(let j = 0 ; j < voteRecordEntries.length;j++){
+                    if(voteEntries[i].vote === voteRecordEntries[j].docId){
+                      voteEntries[i].billId = voteRecordEntries[j].data.bill
+                      voteEntries[i].yeas = voteRecordEntries[j].data.yeas
+                      voteEntries[i].nays = voteRecordEntries[j].data.nays
+                    }
+                  }
+                }
+                let bills = db.Bill()
+                let billClassif = db.BillClassification()
+                let records = bills.innerJoin('_id',billClassif,'bill').then(result =>{
+                  for(let i = 0 ; i < voteEntries.length;i++){
+                    for(let j = 0 ; j < result.length;j++){
+                      if(voteEntries[i].billId === result[j].bill){
+                        voteEntries[i].billInfo = result[j]
+                      }
+                    }
+                  }
+                  res.json({
+                    success: true,
+                    data: voteEntries
+                  })
+                })
+              })
+            })
+      })
 }
