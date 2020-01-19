72f3083db9112689561688a1de8e3b9cd936f9a8
khalid baraka <baraka.khalid4@gmail.com>
2020-01-19 02:15:02 -0500

    created the backend for fetching the bills using inner join #133
    with @ishmam22

--- controllers/BillController.js
+++ controllers/BillController.js
@@ -1,3 +1,57 @@
+import { Firestore } from '../client/src/Firebase'
+
 exports.getBillById = (req, res) => {
   console.log('getBillById endpoint was successfully callled')
+}
+
+exports.getAllBillsByRep = (req, res) =>  {
+  console.log('getAllBillsByHead endpoint was successfully callled')
+  let records = []
+  const db = new Firestore()
+  let votes = db.Vote()
+  let representative = req.params.head.toLowerCase()
+    console.log('iM HERE !!!!!!!! '+representative)
+  let voteRecord = db.VoteRecord()
+  let bills = db.Bill()
+  let billClassification = db.BillClassification()
+
+  let finalArray =[]
+  let test = []
+  let repId = ""
+  db.Politician()
+      .select('name', '==', representative)
+      .then(snapshot => {
+        if (snapshot.empty) {
+          res.status(400).json({
+            message: 'Rep Not Found',
+            success: false
+          })
+        }
+        snapshot.forEach(doc => {
+          repId = doc.id
+        })
+        votes.where('member','==',repId).innerJoin('vote',voteRecord,'_id').then(result=>{
+          result.forEach(element => {
+            test.push(element)
+          })
+          if(result){
+            bills.innerJoin('_id',billClassification,'bill').then( billTable=>{
+              for(let i= 0; i<result.length; i++){
+                for(let j= 0; j<billTable.length; j++){
+
+                  if(result[i].bill == billTable[j].bill){
+                    let temp = {voteRecord: result[i],billData: billTable[j]}
+                    console.log(temp)
+                    finalArray.push(temp)
+                  }
+                }}
+              res.json({
+                success: true,
+                data: finalArray
+              })
+            })}
+
+        })
+
+      })
 }
