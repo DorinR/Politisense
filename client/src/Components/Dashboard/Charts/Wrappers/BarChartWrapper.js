d10a76d8e1518985adf9468a25b56f06f8b01ad4
khalid baraka <baraka.khalid4@gmail.com>
2020-01-19 02:13:14 -0500

    updating the wrapper for linking the data with piechart d3 #133

--- client/src/Components/Dashboard/Charts/Wrappers/BarChartWrapper.js
+++ client/src/Components/Dashboard/Charts/Wrappers/BarChartWrapper.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import BarPieChart from '../BarPieCharts'
 import BarChart from '../BarChart'
 import DonutChart from '../DonutChart'
+import D3Chart from "../D3Chart";
 export default class BarChartWrapper extends Component {
   componentDidMount () {
     switch (this.props.type) {
@@ -9,7 +10,7 @@ export default class BarChartWrapper extends Component {
         return new BarPieChart(this.refs.chart)
       case 'bar':
         return new BarChart(this.refs.chart)
-      default: return new DonutChart(this.refs.chart)
+      default: return new D3Chart(this.refs.chart,this.props.data,this.props.categoryType)
     }
   }
 
