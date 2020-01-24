987084c20cec202e5236f261b53f5f2ec15da0d6
khalid baraka <baraka.khalid4@gmail.com>
2020-01-24 01:23:01 -0500

    npx standard --fix #133

--- client/src/Components/Dashboard/PastBills/BillHistoryTable.js
+++ client/src/Components/Dashboard/PastBills/BillHistoryTable.js
@@ -47,93 +47,93 @@ const useStyles = makeStyles({
 export async function fetchUserRiding (userEmail) {
   let result = ''
   await axios
-      .get(`http://localhost:5000/api/users/${userEmail}/getUser`)
-      .then(res => {
-        if (res.data.success) {
-          const riding = res.data.data.riding
-          result = riding
-        }
-      })
-      .catch(err => console.log(err))
+    .get(`http://localhost:5000/api/users/${userEmail}/getUser`)
+    .then(res => {
+      if (res.data.success) {
+        const riding = res.data.data.riding
+        result = riding
+      }
+    })
+    .catch(err => console.log(err))
   return result
 }
 
 export async function fetchRepresentative (riding) {
   let result = ''
   await axios
-      .get(
+    .get(
           `http://localhost:5000/api/representatives/${riding}/getRepresentative`
-      )
-      .then(res => {
-        if (res.data.success) {
-          const representative = res.data.data.name
-          result = representative
-        }
-      })
-      .catch(err => console.log(err))
+    )
+    .then(res => {
+      if (res.data.success) {
+        const representative = res.data.data.name
+        result = representative
+      }
+    })
+    .catch(err => console.log(err))
   return result
 }
 
 export async function fetchRepresentatieVotes (representative) {
   const result = []
   await axios
-      .get(
+    .get(
           `http://localhost:5000/api/voteRecord/getVotesByRepresentative/${representative}`
-      )
-      .then(res => {
-        if (res.data.success) {
-          const votes = res.data.data
-          votes.forEach(vote => result.push(vote))
-        }
-      })
-      .catch(err => console.log(err))
+    )
+    .then(res => {
+      if (res.data.success) {
+        const votes = res.data.data
+        votes.forEach(vote => result.push(vote))
+      }
+    })
+    .catch(err => console.log(err))
   return result
 }
 
 export function fetchAllBills () {
   return axios
-      .get('http://localhost:5000/api/bills/getAllBills')
-      .then(res => {
-        if (res.data.success) {
-          return res.data.data
-        }
-      })
-      .catch(console.error)
+    .get('http://localhost:5000/api/bills/getAllBills')
+    .then(res => {
+      if (res.data.success) {
+        return res.data.data
+      }
+    })
+    .catch(console.error)
 }
 
 export async function fetchRepresentativeId (representative) {
   return axios
-      .get(
+    .get(
           `http://localhost:5000/api/representatives/${representative}/getRepresentativeId`
-      )
-      .then(res => {
-        if (res.data.success) {
-          return res.data.data
-        }
-      })
-      .catch(console.error)
+    )
+    .then(res => {
+      if (res.data.success) {
+        return res.data.data
+      }
+    })
+    .catch(console.error)
 }
 
 export async function fetchRepresentativeVotes (representativeId) {
   return axios
-      .get(
+    .get(
           `http://localhost:5000/api/votes/${representativeId}/getAllVotesByRepresentative`
-      )
-      .then(res => {
-        if (res.data.success) {
-          return res.data.data
-        }
-      })
+    )
+    .then(res => {
+      if (res.data.success) {
+        return res.data.data
+      }
+    })
 }
 
 export async function fetchAllVoteRecords () {
   return axios
-      .get('http://localhost:5000/api/voteRecord/getAllVoteRecords')
-      .then(res => {
-        if (res.data.success) {
-          return res.data.data
-        }
-      })
+    .get('http://localhost:5000/api/voteRecord/getAllVoteRecords')
+    .then(res => {
+      if (res.data.success) {
+        return res.data.data
+      }
+    })
 }
 
 function generateTableRows (bills) {
@@ -141,11 +141,11 @@ function generateTableRows (bills) {
   bills.forEach(bill => {
     const { number, dateVoted, title, sponsorName, link, vote } = bill
     const tableRow = createData(
-        number,
-        dateVoted,
-        title,
-        vote,
-        <BillDetails title={title} sponsor={sponsorName} linkToFullText={link} />
+      number,
+      dateVoted,
+      title,
+      vote,
+      <BillDetails title={title} sponsor={sponsorName} linkToFullText={link} />
     )
     rows.push(tableRow)
   })
@@ -154,9 +154,9 @@ function generateTableRows (bills) {
 function assembleBillObjects (bills, voteRecords, votesByRepresentative) {
   bills.forEach(bill => {
     bill.vote = getRepresentativeVote(
-        bill.id,
-        voteRecords,
-        votesByRepresentative
+      bill.id,
+      voteRecords,
+      votesByRepresentative
     )
   })
 
@@ -202,14 +202,14 @@ export default function BillHistoryTable () {
       const representative = await fetchRepresentative(riding)
       const representativeId = await fetchRepresentativeId(representative)
       const votesByRepresentative = await fetchRepresentativeVotes(
-          representativeId
+        representativeId
       )
       const bills = await fetchAllBills()
       const voteRecords = await fetchAllVoteRecords()
       const fullBills = assembleBillObjects(
-          bills,
-          voteRecords,
-          votesByRepresentative
+        bills,
+        voteRecords,
+        votesByRepresentative
       )
       generateTableRows(fullBills)
     }
@@ -226,59 +226,59 @@ export default function BillHistoryTable () {
   }
 
   return (
-      <Paper className={classes.root}>
-        <div className={classes.tableWrapper}>
-          <Table stickyHeader>
-            <TableHead>
-              <TableRow>
-                {columns.map(column => (
-                    <TableCell
-                        key={column.id}
-                        align={column.align}
-                        style={{ minWidth: column.minWidth }}
-                    >
-                      {column.label}
-                    </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows
-                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map(row => {
-                    return (
-                        <TableRow hover role='checkbox' tabIndex={-1} key={row.code}>
-                          {columns.map(column => {
-                            const value = row[column.id]
-                            return (
-                                <TableCell key={column.id} align={column.align}>
-                                  {column.format && typeof value === 'number'
-                                      ? column.format(value)
-                                      : value}
-                                </TableCell>
-                            )
-                          })}
-                        </TableRow>
-                    )
-                  })}
-            </TableBody>
-          </Table>
-        </div>
-        <TablePagination
-            rowsPerPageOptions={[10, 25, 100]}
-            component='div'
-            count={rows.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            backIconButtonProps={{
-              'aria-label': 'previous page'
-            }}
-            nextIconButtonProps={{
-              'aria-label': 'next page'
-            }}
-            onChangePage={handleChangePage}
-            onChangeRowsPerPage={handleChangeRowsPerPage}
-        />
-      </Paper>
+    <Paper className={classes.root}>
+      <div className={classes.tableWrapper}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map(row => {
+                return (
+                  <TableRow hover role='checkbox' tabIndex={-1} key={row.code}>
+                    {columns.map(column => {
+                      const value = row[column.id]
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      )
+                    })}
+                  </TableRow>
+                )
+              })}
+          </TableBody>
+        </Table>
+      </div>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component='div'
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        backIconButtonProps={{
+          'aria-label': 'previous page'
+        }}
+        nextIconButtonProps={{
+          'aria-label': 'next page'
+        }}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
   )
 }
