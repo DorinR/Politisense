960d4d245ea3706d0e7dd988dfdc94f773064d26
Ishmam Murtaza <ishmammurtaza@gmail.com>
2020-02-08 00:16:57 -0500

    #201 initial template This reverts commit 78bc7652

--- client/src/Components/Dashboard/GeneralDashboard.js
+++ client/src/Components/Dashboard/GeneralDashboard.js
@@ -1,396 +1,162 @@
-/* eslint-disable */
-import React, { useEffect } from 'react'
-import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card'
-import Typography from '@material-ui/core/Typography'
-import BarChartWrapper from './Charts/Wrappers/BarChartWrapper'
-import Radar from 'react-d3-radar'
-import axios from 'axios'
-import { fetchUserRiding } from '../Navbar'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import { makeStyles } from '@material-ui/core/styles'
-import CardContent from '@material-ui/core/CardContent'
-import NotListedLocationIcon from '@material-ui/icons/NotListedLocation'
-import Box from '@material-ui/core/Box'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import Avatar from '@material-ui/core/Avatar'
-import ListItemText from '@material-ui/core/ListItemText'
-import List from '@material-ui/core/List'
-import { capitalizedName } from './BillDialog'
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import StarIcon from '@material-ui/icons/StarBorder';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import HelpIcon from '@material-ui/icons/Help';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import EventSeatIcon from '@material-ui/icons/EventSeat';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 
-const useStyles = makeStyles({
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+    },
+  },
   card: {
-    minWidth: 275
+    minWidth: 275,
   },
   bullet: {
     display: 'inline-block',
     margin: '0 2px',
-    transform: 'scale(0.8)'
-  },
-  pos: {
-    marginBottom: 12
+    transform: 'scale(0.8)',
   },
   title: {
-    fontSize: 18,
-    textAlign: 'center'
+    fontSize: 14,
   },
-  MuiAvatar: {
-    backgroundColor: '#43D0C4'
+  pos: {
+    marginBottom: 12,
   },
-  container: {
-    margin: '20px',
-    marginTop: '30px'
-  }
-})
+  navbarCustomFont: {
+    color: '#D71921',
+    fontWeight: 'bold',
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  cardHeader: {
+    backgroundColor:
+        theme.palette.type === 'dark' ? theme.palette.grey[700] : theme.palette.grey[200],
+  },
+  cardPricing: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'baseline',
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
+  },
+}));
 
-export default function CategoryDashboard() {
-  const classes = useStyles()
-  const [categoryList] = React.useState([
-    'economics',
-    'healthcare',
-    'human rights',
-    'business',
-    'religion',
-    'criminal',
-    'trade'
-  ])
-  const [userRepresentative, setUserRepresentative] = React.useState('')
-  const [representativeData, setRepresentativeData] = React.useState([])
-  const [radarData, setRadarData] = React.useState([])
-  const [repDataLoaded, setRepDataLoaded] = React.useState(false)
-  const [donutData, setDonutData] = React.useState([])
-  const [reps, setReps] = React.useState([])
-  const [userRepIssuedBills, setUserRepIssuedBills] = React.useState([])
+export default function Pricing() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
 
-  useEffect(() => {
-    async function getDataForDonut() {
-      if (reps.length && representativeData.length) {
-        const data = await createDataSetDonut(reps, representativeData)
-        if (
-          data.Liberal !== 0 &&
-          data.Liberal !== 0 &&
-          data.Liberal !== 0 &&
-          data.Liberal !== 0 &&
-          data.Liberal !== 0 &&
-          data.Liberal !== 0
-        ) {
-          setDonutData([data])
-        }
-      }
-    }
-    async function getAllReps() {
-      let result = []
-      await axios
-        .get('http://localhost:5000/api/representatives/getAllRepresentatives')
-        .then(res => {
-          if (res.data.success) {
-            result = res.data.data
-          }
-        })
-        .catch(err => console.error(err))
-      return result
-    }
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    async function getAllBillsByRep(head) {
-      let result = []
-      await axios
-        .get(`http://localhost:5000/api/bills/${head}/getAllBillsByRep`)
-        .then(res => {
-          if (res.data.success) {
-            result = res.data.data
-            setRepresentativeData(result)
-            setRepDataLoaded(true)
-          }
-        })
-        .catch(err => console.error(err))
-      return result
-    }
-    async function getAllBillsBySponsorName(head) {
-      let result = []
-      await axios
-        .get(`http://localhost:5000/api/bills/${head}/getAllBillsBySponsorName`)
-        .then(res => {
-          if (res.data.success) {
-            result = res.data.data
-          }
-        })
-        .catch(err => console.error(err))
-      return result
-    }
-    async function getData() {
-      const user = JSON.parse(localStorage.getItem('user'))
-      if (user) {
-        const { email } = user
-        const riding = await fetchUserRiding(email)
-        const representative = await fetchRepresentative(riding)
-        const allRepresentatives = await getAllReps()
-        setUserRepresentative(representative)
-        setReps(allRepresentatives)
-        const issuedBillByUserRep = await getAllBillsBySponsorName(
-          representative
-        )
-        setUserRepIssuedBills(issuedBillByUserRep)
-        if (representative.length !== 0) {
-          setUserRepresentative(representative)
-        }
-      }
-    }
-    getData()
-
-    if (userRepresentative) {
-      getAllBillsByRep(userRepresentative).then(results => {})
-    }
-
-    if (categoryList && repDataLoaded) {
-      createDataSetRadar(categoryList, representativeData).then(testing => {
-        if (testing.length !== 0) {
-          setRadarData(testing)
-        }
-      })
-    }
-
-    if (reps.length && repDataLoaded) {
-      getDataForDonut()
-    }
-  }, [userRepresentative, repDataLoaded])
-
-  async function fetchRepresentative(riding) {
-    let result = ''
-    await axios
-      .get(
-        `http://localhost:5000/api/representatives/${riding}/getRepresentative`
-      )
-      .then(res => {
-        if (res.data.success) {
-          result = res.data.data.name
-        }
-      })
-      .catch(err => console.error(err))
-    return result
-  }
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <div className={classes.container}>
-      <Grid container spacing={1}>
-        <Grid item xs={12}>
-          {userRepIssuedBills.length !== 0 && categoryList.length !== 0 ? (
-            <Card>
-              <CardContent>
-                <Typography className={classes.title}>
-                  Bills sponsored by {capitalizedName(userRepresentative)}
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="sm" component="main" className={classes.heroContent}>
+          <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            Your Government
+          </Typography>
+        </Container>
+        {/* End hero unit */}
+        <Container maxWidth="md" component="main">
+          <Grid container spacing={5} alignItems="flex-end">
+            <Grid item xs={4}>
+              <Container>
+                <Typography variant="h5" align="center" color="textPrimary" className={classes.current}>
+                  Current Government
                 </Typography>
-                <BarChartWrapper
-                  type='bar-pie'
-                  data={userRepIssuedBills}
-                  categories={categoryList}
-                />
-                <Box border mx='auto'>
-                  <List>
-                    <ListItem>
-                      <ListItemAvatar>
-                        <Avatar className={classes.avatar}>
-                          <NotListedLocationIcon />
-                        </Avatar>
-                      </ListItemAvatar>
-                      <ListItemText>
-                        The distribution of issued bills by the representative
-                        among different categories
-                      </ListItemText>
-                    </ListItem>
-                  </List>
-                </Box>
-              </CardContent>
-            </Card>
-          ) : (
-            ''
-          )}
-        </Grid>
-
-        {radarData.length !== 0 && categoryList.length !== 0 ? (
-          <Grid item xs={12}>
-            <Grid container spacing={2}>
-              <Grid item xs={6}>
-                <Card>
+                <Typography variant="h5" align="center" color="textPrimary" className={classes.navbarCustomFont}>
+                  Liberal Minority <HelpIcon color="primary" onClick={handleClickOpen}/>
+                </Typography>
+              </Container>
+            </Grid>
+            <Grid item xs={8}>
+              <Container >
+                <Card className={classes.card}>
                   <CardContent>
-                    <Typography className={classes.title}>
-                      MP Voting Distribution
+                    <Typography className={classes.title} gutterBottom>
+                      Seats in Parliament
                     </Typography>
-                    <Radar
-                      width={400}
-                      height={350}
-                      padding={40}
-                      domainMax={radarData[1] + 3}
-                      highlighted
-                      onHover={point => {
-                        if (point) {
-                        } else {
-                        }
-                      }}
-                      data={{
-                        variables: [
-                          { key: 'trade', label: 'Trade' },
-                          { key: 'criminal', label: 'Criminal' },
-                          { key: 'business', label: 'Business' },
-                          { key: 'Economics', label: 'Economics' },
-                          { key: 'Healthcare', label: 'Healthcare' },
-                          { key: 'Religion', label: 'Religion' },
-                          { key: 'Human Rights', label: 'Human Rights' }
-                        ],
-                        sets: [
-                          {
-                            values: radarData[0]
-                          }
-                        ]
-                      }}
-                    />
-                    <Box border mx='auto'>
-                      <List>
-                        <ListItem>
-                          <ListItemAvatar>
-                            <Avatar className={classes.avatar}>
-                              <NotListedLocationIcon />
-                            </Avatar>
-                          </ListItemAvatar>
-                          <ListItemText>
-                            This radar chart shows the MP's activity with
-                            respect to a variety categories.
-                          </ListItemText>
-                        </ListItem>
-                      </List>
-                    </Box>
+                    {Array.apply(null, { length: 31 }).map((e, i) => (
+                        <span className="busterCards" key={i}><EventSeatIcon style={{ fontSize: 30, color:'#D71921'}} /></span>))}
+                    {Array.apply(null, { length: 24 }).map((e, i) => (
+                        <span className="busterCards" key={i}><EventSeatIcon style={{ fontSize: 30, color:'#0C499C'}} /></span>))}
+                    {Array.apply(null, { length: 6   }).map((e, i) => (
+                        <span className="busterCards" key={i}><EventSeatIcon style={{ fontSize: 30, color:'#EF7E52'}} /></span>))}
+                    {Array.apply(null, { length: 5 }).map((e, i) => (
+                        <span className="busterCards" key={i}><EventSeatIcon style={{ fontSize: 30, color:'#EF7E52'}} /></span>))}
+                    <EventSeatIcon style={{ fontSize: 30, color:'#2E8724'}} />
+                    <EventSeatIcon style={{ fontSize: 30, color:'#676767'}} />
                   </CardContent>
+                  <CardActions>
+                    <Button size="small">Learn More</Button>
+                  </CardActions>
                 </Card>
-              </Grid>
-              {donutData.length ? (
-                <Grid item item xs={6}>
-                  <Card>
-                    <CardContent>
-                      <Typography className={classes.title}>
-                        Bipartisan Index
-                      </Typography>
-                      <BarChartWrapper type='donut' data={donutData} />
-                      <Box border mx='auto'>
-                        <List>
-                          <ListItem>
-                            <ListItemAvatar>
-                              <Avatar className={classes.avatar}>
-                                <NotListedLocationIcon />
-                              </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText>
-                              The Bipartisan Index measures how often a member
-                              of Parliamnet introduces bills that succeed in
-                              attracting co-sponsors from members of the other
-                              party, and how often they in turn co-sponsor a
-                              bill introduced from across the aisle.
-                            </ListItemText>
-                          </ListItem>
-                        </List>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              ) : (
-                ''
-              )}
+              </Container>
             </Grid>
           </Grid>
-        ) : (
-          <div
-            style={{
-              position: 'absolute',
-              left: '50%',
-              top: '50%',
-              zIndex: '-2',
-              transform: 'translate(-50%, -50%)'
-            }}>
-            <CircularProgress />
-          </div>
-        )}
-      </Grid>
-    </div>
-  )
-}
-
-export async function createDataSetRadar(categories, data) {
-  const dataArray = []
-  let temp = {}
-  const dataSetRadar = {}
-  let maxValue = 0
-  categories.forEach(category => {
-    let totalvotes = 0
-    data.forEach(bill => {
-      if (bill.billData.category === category.toLowerCase()) {
-        totalvotes++
-      }
-    })
-    const categorySmallLetter = category.toLowerCase().trim()
-    temp = { category: categorySmallLetter, value: totalvotes }
-    dataArray.push(temp)
-  })
-
-  dataArray.forEach(category => {
-    if (category.value > maxValue) {
-      maxValue = category.value
-    }
-    if (category.category) {
-      dataSetRadar[category.category] = category.value
-    }
-  })
-
-  return [dataSetRadar, maxValue]
-}
-
-export async function createDataSetDonut(sponsors, mpdata) {
-  let liberalCounter = 0
-  let conservativeCounter = 0
-  let ndpCounter = 0
-  let peopleCounter = 0
-  let greenCounter = 0
-  let bqCounter = 0
-  let parties = {}
-
-  if (mpdata.length) {
-    mpdata.forEach(bill => {
-      if (bill.voteRecord.yea === true) {
-        sponsors.forEach(sponsor => {
-          if (sponsor.name === bill.billData.sponsorName) {
-            switch (sponsor.politicalParty) {
-              case 'liberal':
-                liberalCounter++
-                break
-              case 'conservative':
-                conservativeCounter++
-                break
-              case 'ndp':
-                ndpCounter++
-                break
-              case 'bloc québécois':
-                bqCounter++
-                break
-              case 'green':
-                greenCounter++
-                break
-              case 'people':
-                peopleCounter++
-                break
-              default:
-                return 'nothing'
-            }
-          }
-        })
-      }
-    })
-  }
-
-  parties = {
-    Liberal: liberalCounter,
-    Conservative: conservativeCounter,
-    NDP: ndpCounter,
-    People: peopleCounter,
-    Green: greenCounter,
-    BQ: bqCounter
-  }
-  return parties
+        </Container>
+        <Dialog
+            open={open}
+            TransitionComponent={Transition}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-slide-title"
+            aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle >{"What is a minority government?"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-slide-description">
+              In Canada's parliamentary system of responsible government, minority governments occur when no party has a majority of seats in the legislature.
+              Typically, but not necessarily, the party with a plurality of seats forms the government.
+              In a minority situation, governments must rely on the support of other parties to stay in power, providing less stability than a majority government.
+              In Canada, political parties rarely form official coalition governments to form a majority.
+            </DialogContentText>
+          </DialogContent>
+        </Dialog>
+      </React.Fragment>
+  );
 }
