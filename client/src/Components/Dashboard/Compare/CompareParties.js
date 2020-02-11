56959269276bc09c2ccf0f6a6deab1fc2a6d87c0
DorinR <dorin.rogov@gmail.com>
2020-02-10 23:47:55 -0500

    #219 created InfoBubble component to display useful information for the user

--- client/src/Components/Dashboard/Compare/CompareParties.js
+++ client/src/Components/Dashboard/Compare/CompareParties.js
@@ -8,7 +8,7 @@ import Grid from '@material-ui/core/Grid'
 import Container from '@material-ui/core/Container'
 import D3ChartHeadVsHeadContainer from '../D3ChartHeadVsHeadContainer'
 import Grow from '@material-ui/core/Grow'
-import Tooltip from '../InfoTooltip/Tooltip'
+import InfoBubble from '../InfoTooltip/InfoBubble'
 
 const useStyles = makeStyles(theme => ({
   content: {
@@ -16,11 +16,9 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(8, 0, 6)
   },
   customHeaders: {
-    color: '#42AAA8',
     fontWeight: 'bold',
     fontStyle: 'italic',
     fontSize: '3em',
-    textDecoration: 'underline',
     marginLeft: '-46px'
   },
   customTooltip: {
@@ -29,17 +27,17 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-export async function getAllBillsByHead (head) {
+export async function getAllBillsByHead(head) {
   const res = await axios.get(
     `http://localhost:5000/api/bills/${head}/getAllBillsByHead`
   )
   return res.data.data
 }
-export function calcPercent (percent) {
+export function calcPercent(percent) {
   return [percent, 100 - percent]
 }
 
-export default function CompareRepresentatives () {
+export default function CompareRepresentatives() {
   const classes = useStyles()
   const [head1, setHead1] = useState('')
   const [head2, setHead2] = useState('')
@@ -62,7 +60,7 @@ export default function CompareRepresentatives () {
   }
 
   useEffect(() => {
-    async function getalldata (dataForHead1, dataForHead2) {
+    async function getalldata(dataForHead1, dataForHead2) {
       let dataset = {}
       let commonBillsCounter = 0
       let similarities = 0
@@ -88,7 +86,7 @@ export default function CompareRepresentatives () {
       return [dataset, final]
     }
 
-    async function getBills () {
+    async function getBills() {
       const head1Bills = await getAllBillsByHead(head1, 'head1')
       const head2Bills = await getAllBillsByHead(head2, 'head2')
       const dataset = await getalldata(head1Bills, head2Bills)
@@ -100,7 +98,8 @@ export default function CompareRepresentatives () {
     }
   }, [head1, head2])
 
-  const titleExplanation = 'Select two Parties of your choice and compare their information'
+  const titleExplanation =
+    'Select two Parties of your choice and compare their information'
 
   return (
     <>
@@ -112,21 +111,22 @@ export default function CompareRepresentatives () {
               style={{ display: 'inline-block' }}
               className={classes.customHeaders}
               align='left'
-              color='textPrimary'
-              gutterBottom
-            >
+              color='primary'
+              gutterBottom>
               Compare Parties
             </Typography>
             <span className={classes.customTooltip}>
-              <Tooltip text={titleExplanation} size='large' />
+              <InfoBubble
+                title={'What is a minory government?'}
+                text={titleExplanation}
+              />
             </span>
           </Container>
           <Typography
             variant='h5'
             align='center'
             color='textSecondary'
-            paragraph
-          >
+            paragraph>
             Select two MP's of your choice and compare their information and
             performance in terms of bills sponsored and voted pm and then see
             how much or how little they agree on!
---
+++ client/src/Components/Dashboard/InfoTooltip/InfoBubble.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import HelpIcon from '@material-ui/icons/Help'
+import { makeStyles } from '@material-ui/core/styles'
+import Dialog from '@material-ui/core/Dialog'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Slide from '@material-ui/core/Slide'
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction='up' ref={ref} {...props} />
+})
+
+const useStyles = makeStyles(theme => ({
+  help: {
+    cursor: 'pointer'
+  }
+}))
+
+export default function InfoBubble(props) {
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <span>
+      <HelpIcon
+        className={classes.help}
+        color='primary'
+        onClick={handleClickOpen}
+      />
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        onClose={handleClose}>
+        <DialogTitle>{props.title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{props.text}</DialogContentText>
+        </DialogContent>
+      </Dialog>
+    </span>
+  )
+}
