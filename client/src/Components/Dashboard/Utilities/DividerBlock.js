600dfe2fef7d5bc379bad1a01b2985f6ed7f9edb
DorinR <dorin.rogov@gmail.com>
2020-02-22 15:59:37 -0500

    #219 added info bubble to finances subsection, added dynamic text color based on party

--- client/src/Components/Dashboard/Utilities/DividerBlock.js
+++ client/src/Components/Dashboard/Utilities/DividerBlock.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
+import InfoBubble from './InfoBubble'
 
 const useStyles = makeStyles(theme => ({
   customBackground: {
@@ -11,20 +12,32 @@ const useStyles = makeStyles(theme => ({
   customTypographyFont: {
     color: 'white',
     fontWeight: 'bold'
+  },
+  infoBubblePlacement: {
+    borderTop: '1px solid transparent'
   }
 }))
 
-export default function DividerBlock (props) {
+export default function DividerBlock(props) {
   const classes = useStyles()
 
   return (
     <div
       className={classes.customBackground}
-      style={{ backgroundColor: props.color }}
-    >
-      <Typography variant='h6' className={classes.customTypographyFont}>
-        {props.text}
-      </Typography>
+      style={{ backgroundColor: props.color }}>
+      <span style={{ display: '-webkit-inline-box' }}>
+        <Typography variant='h6' className={classes.customTypographyFont}>
+          {props.text}
+        </Typography>
+        <span className={classes.infoBubblePlacement}>
+          {props.infoBubbleText && (
+            <InfoBubble
+              title={props.infoBubbleTitle}
+              text={props.infoBubbleText}
+            />
+          )}
+        </span>
+      </span>
     </div>
   )
 }
