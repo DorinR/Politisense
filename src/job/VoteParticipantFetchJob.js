82151a1826ea0c56f5dc88d267c4f441883df8e6
mathewplouffe <m_plo@encs.concordia.ca>
2020-02-29 00:08:46 -0500

    #207 [feature/voters-migration] : implemented VoteParticipantFetchJob

---
+++ src/job/VoteParticipantFetchJob.js
@@ -0,0 +1,27 @@
+const Job = require('../util/Job').AbstractJob
+const Actions = require('@action')
+const Parsers = require('@parser')
+
+class VoteParticipantFetchJob extends Job {
+  static create (params, cb) {
+    return new VoteParticipantFetchJob(params, cb)
+      .addAction(new Actions.FetchAction(VoteParticipantFetchJob.createRequestParams(params)))
+      .addAction(new Actions.ParserWrapperAction(Parsers.VoteParticipantsXmlParser, {id: params.id}))
+      .addAction(new Actions.FormatAction(params))
+      .addErrorAction(new Actions.HandleConnectionErrorAction(cb, VoteParticipantFetchJob.create, params))
+  }
+
+  constructor (params, cb) {
+    super(params.url, cb)
+    this.params = params
+  }
+
+  static createRequestParams (params) {
+    return {
+      url: params.url,
+      params: params.params
+    }
+  }
+}
+
+module.exports.VoteParticipantFetchJob = VoteParticipantFetchJob
--- src/job/jobs.js
+++ src/job/jobs.js
@@ -4,3 +4,4 @@ module.exports.ScrapeJob = require('./ScrapeJob').ScrapeJob
 module.exports.PoliticianFetchJob = require('./PoliticianFetchJob').PoliticianFetchJob
 module.exports.RoleFetchJob = require('./RoleFetchJob').RoleFetchJob
 module.exports.VoteRecordFetchJob = require('./VoteRecordFetchJob').VoteRecordFetchJob
+module.exports.VoteParticipantFetchJob = require('./VoteParticipantFetchJob').VoteParticipantFetchJob
