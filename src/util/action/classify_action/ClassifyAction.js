c77ba50327a6db19604ac301826abbcdcb3e9e6f
mathewplouffe <m_plo@encs.concordia.ca>
2020-03-01 12:46:50 -0500

    #242 [feature/classification-refactor] : implemented a classify action

---
+++ src/util/action/classify_action/ClassifyAction.js
@@ -0,0 +1,93 @@
+const Action = require('../JobAction').AbstractJobAction
+const proc = require('child_process')
+const fs = require('fs')
+const path = require('path')
+
+class ClassificationAction extends Action {
+  constructor (fp) {
+    super()
+    this.classifierPath = fp ? fp : path.join(__dirname,'classifier.py')
+  }
+
+  async perform (billParams) {
+    return await this.addBillsToClassifier(billParams)
+  }
+
+  addBillsToClassifier (billParams) {
+    const { execString, paths, params } = this.createExternalClassifierParameters(billParams)
+    return this.runExternalClassifier(execString, paths, params)
+  }
+
+  createExternalClassifierParameters (billParams) {
+    let execString = 'python3.7 ' + this.classifierPath
+    const paths = []
+    const params = []
+    billParams.forEach((billParam, i) => {
+      const path = this.createTempFile(i, billParam)
+      paths.push(path)
+      params.push({
+        id: billParam.params.id,
+        parliament: billParam.params.parliament
+      })
+      execString += ' ' + path
+    })
+    return { execString, paths, params }
+  }
+
+  createTempFile (index, content) {
+    const fp =  path.join(__dirname,`${index}.json`)
+    const out = JSON.stringify({
+      name: content.params.id,
+      content: content.data
+    })
+    fs.writeFileSync(fp, out)
+    return fp
+  }
+
+  runExternalClassifier (execString, paths, params) {
+    return new Promise((resolve, reject) => {
+      const child = proc.exec(execString)
+      child.stdout.pipe(process.stdout)
+      child.stderr.pipe(process.stderr)
+      child.on('exit', (code) => {
+        if (code === 0) {
+          console.log('--------------------------------------------------------------------------------------------------------')
+          console.log('INFO: successfully classified data')
+          console.log('--------------------------------------------------------------------------------------------------------')
+          const raws = this.readClassificationFile(path.join(__dirname,'classifications.json'))
+          const models = this.addToModels(raws, params)
+          this.deleteTempFiles(paths)
+          resolve(models)
+        }
+        reject(new Error(`ERROR: exited with nonzero status: ${code}`))
+      })
+    })
+  }
+
+  readClassificationFile (path) {
+    const file = fs.readFileSync(path)
+    return JSON.parse(file.toString())
+  }
+
+  addToModels (raws, bills) {
+    const classifiedBills = []
+    bills.forEach(({id, parliament}) => {
+      classifiedBills.push({
+        bill: id,
+        parliament: parliament,
+        raw: raws[id]
+      })
+    })
+    return classifiedBills
+  }
+
+  deleteTempFiles (files) {
+    fs.unlinkSync(path.join(__dirname,'classifications.json'))
+    files.forEach(path => {
+      console.debug(`INFO: deleting temporary file: ${path} on completion`)
+      fs.unlinkSync(path)
+    })
+  }
+}
+
+module.exports.ClassificationAction = ClassificationAction
