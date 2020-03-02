756bba84b60386229110df48d32dbd424891c3c1
DorinR <dorin.rogov@gmail.com>
2020-03-01 21:39:48 -0500

    #162 adding configuration to server.js file

--- server.js
+++ server.js
@@ -32,5 +32,9 @@ app.use(
 
 // deployment configuration
 const PORT = process.env.PORT || 5000
+app.use(express.static(path.join(__dirname, './client/build')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + './client/build/index.html'))
+})
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
