f225247e65bf6d1c8e9b105f38f1fa4e1de1e58e
TheAlexRobert <alexandrerobertsmeets@gmail.com>
2020-03-06 17:31:54 -0500

    #246 created a row of bills with title, link and date

--- client/src/Components/Dashboard/Polls/PollsMainPage.js
+++ client/src/Components/Dashboard/Polls/PollsMainPage.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Poll from 'react-polls';
 import Card from 'react-bootstrap/Card'
-import ListGroup from 'react-bootstrap/ListGroup'
+import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
 
 
 // Declaring poll question and answers
@@ -36,36 +36,53 @@ class PollsMainPage extends Component {
         return (
             <div>
                 {/* <Poll question={pollQuestion} answers={pollAnswers} onVote={this.handleVote} /> */}
-                <Card style={{ width: '18rem' }}>
-                    <Card.Body>
-                        <Card.Title>Bill C-130</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                <Container>
+                    <Row>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <Card.Title>Bill C-130</Card.Title>
+                                <Card.Link href="#">Bill Link</Card.Link>
+                                <Card.Text>
+                                    <small className="text-muted">12 December 2019</small>
+                                </Card.Text>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
                     </Card.Text>
-                        <Poll answers={pollAnswers} onVote={this.handleVote} />
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Body>
-                        <Card.Title>Bill D-76</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                                <Poll answers={pollAnswers} onVote={this.handleVote} />
+                            </Card.Body>
+                        </Card>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <Card.Title>Bill D-76</Card.Title>
+                                <Card.Link href="#">Bill Link</Card.Link>
+                                <Card.Text>
+                                    <small className="text-muted">1 November 2019</small>
+                                </Card.Text>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
                     </Card.Text>
-                        <Poll answers={pollAnswers} onVote={this.handleVote} />
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Body>
-                        <Card.Title>Motion 30</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                    </Card.Text>
-                        <Poll answers={pollAnswers} onVote={this.handleVote} />
-                    </Card.Body>
-                </Card>
+                                <Poll answers={pollAnswers} onVote={this.handleVote} />
+                            </Card.Body>
+                        </Card>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <Card.Title>Motion 30</Card.Title>
+                                <Card.Link href="#">Bill Link</Card.Link>
+                                <Card.Text>
+                                    <small className="text-muted">19 October 2019</small>
+                                </Card.Text>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                </Card.Text>
+                                <Poll answers={pollAnswers} onVote={this.handleVote} />
+                            </Card.Body>
+                        </Card>
+                    </Row>
+
+                </Container>
             </div>
 
         );
