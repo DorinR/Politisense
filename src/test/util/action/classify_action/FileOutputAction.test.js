fcbf69a1b379204133cac2e5bf1a3bd244113a5a
mathewplouffe <m_plo@encs.concordia.ca>
2020-03-06 01:19:55 -0500

    #210 [feature/periodic-updates] : classify action test stubs

---
+++ src/test/util/action/classify_action/FileOutputAction.test.js
@@ -0,0 +1,10 @@
+/* eslint-env jest */
+const chai = require('chai')
+const Assert = chai.assert
+
+describe('FileOutputAction.js', () => {
+  test('', async (done) => {
+    Assert.fail('stub')
+    done()
+  })
+})
