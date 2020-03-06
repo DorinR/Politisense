84930224bbdfcf241fcc3d333fe5d6055a245d78
mathewplouffe <m_plo@encs.concordia.ca>
2020-03-05 20:28:51 -0500

    # 250 [feature/enable-new-data] : new data enabled by default

--- src/firebase/Firestore.js
+++ src/firebase/Firestore.js
@@ -250,11 +250,11 @@ class Firestore {
   }
 
   FinancialRecord () {
-    const collection = this.legacy ? 'financialRecord' : 'financialRecord' // `${this.parliament}/financialRecord` not currently implemented
+    const collection = this.legacy ? 'financialRecord' : 'financialRecord' //`${this.parliament}/financialRecord` not currently implemented
     return this.createReference(collection)
   }
 
-  PoliticalParty () {
+  PoliticalParty() {
     const collection = this.legacy ? 'parties' : 'parties'
     return this.createReference(collection)
   }
@@ -265,7 +265,7 @@ class Firestore {
   }
 
   Riding () {
-    const collection = this.legacy ? 'ridings' : 'ridings'
+    const collection = this.legacy ? 'ridings' : `${this.parliament}/ridings`
     return this.createReference(collection)
   }
 
@@ -280,7 +280,7 @@ class Firestore {
   }
 
   User () {
-    const collection = this.legacy ? 'users' : 'users'
+    const collection = this.legacy ? 'users' : `${this.parliament}/users`
     return this.createReference(collection)
   }
 
@@ -296,6 +296,30 @@ class Firestore {
 
   createReference (collection) {
     return new Reference(this.reference.collection(collection))
+  }
+
+  static copyCollection(from, to) {
+    return new Promise(resolve => {
+      from
+        .select()
+        .then(snapshot => {
+          const records = []
+          snapshot.forEach(doc => {
+            records.push(doc.data())
+          })
+          return records
+        })
+        .then(records => {
+          return records.map(old => {
+            return to.insert(old)
+          })
+        })
+        .then(Promise.all)
+        .then((responses) => {
+          resolve(responses)
+        })
+        .catch(console.error)
+    })
   }
 
   async close () {
