e1a56d74dde3f4aab6abf3e7dc2c4a5e48c20032
TheAlexRobert <alexandrerobertsmeets@gmail.com>
2020-03-06 17:15:25 -0500

    #245 boostrap cards to hold bills and polls

--- client/src/Components/Dashboard/Polls/PollsMainPage.js
+++ client/src/Components/Dashboard/Polls/PollsMainPage.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Poll from 'react-polls';
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+
 
 // Declaring poll question and answers
 const pollQuestion = 'Is react-polls useful?'
 const pollAnswers = [
-    { option: 'Yes', votes: 8 },
-    { option: 'No', votes: 2 }
+    { option: 'Yea', votes: 8 },
+    { option: 'Nay', votes: 2 }
 ]
 
 class PollsMainPage extends Component {
@@ -30,7 +35,37 @@ class PollsMainPage extends Component {
         const { pollAnswers } = this.state
         return (
             <div>
-                <Poll question={pollQuestion} answers={pollAnswers} onVote={this.handleVote} />
+                {/* <Poll question={pollQuestion} answers={pollAnswers} onVote={this.handleVote} /> */}
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Bill C-130</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                    </Card.Text>
+                        <Poll answers={pollAnswers} onVote={this.handleVote} />
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Bill D-76</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                    </Card.Text>
+                        <Poll answers={pollAnswers} onVote={this.handleVote} />
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Motion 30</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                    </Card.Text>
+                        <Poll answers={pollAnswers} onVote={this.handleVote} />
+                    </Card.Body>
+                </Card>
             </div>
 
         );
