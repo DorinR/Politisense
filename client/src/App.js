a4d7dbffdb7ab7754783eca0ef89945778ba2158
khalid baraka <baraka.khalid4@gmail.com>
2020-03-16 14:43:44 -0400

    ridings and provinces are now displayed in the canada Map #233
    added some logic with ridings #234

--- client/src/App.js
+++ client/src/App.js
@@ -12,14 +12,14 @@ import SignUp from './Components/Auth/SignUp'
 import Navbar from './Components/Navbar'
 import Logout from './Components/Logout'
 import UserAccountTabs from './Components/Dashboard/UserAccount/UserAccountTabs'
-import MapWrapper from './Components/Dashboard/MapWrapper'
+import MapWrapper from './Components/Map/MapWrapper'
 import Questionnaire from './Components/Questionnaire'
 import GeneralDashboard from './Components/Dashboard/GeneralDashboard'
 import CategoryDashboard from './Components/Dashboard/CategoryDashboard'
 import BillHistoryTable from './Components/Dashboard/PastBills/BillHistoryTable'
 import BudgetContainer from './Components/Dashboard/Budget/BudgetContainer'
 import CompareContainer from './Components/Dashboard/Compare/CompareContainer'
-
+import MapContainer from "./Components/Map/MapContainer";
 const App = () => {
   const LoginContainer = () => (
     <div className='container'>
@@ -34,7 +34,7 @@ const App = () => {
         <div>
           <Route exact path='/' render={() => <Redirect to='/login' />} />
           <PrivateRoute path='/logout' component={Logout} />
-          <PrivateRoute path='/map' component={MapWrapper} />
+          <PrivateRoute path='/map' component={MapContainer} />
           <PrivateRoute path='/account' component={UserAccountTabs} />
           <PrivateRoute path='/general' component={GeneralDashboard} />
           <PrivateRoute path='/categories' component={CategoryDashboard} />
