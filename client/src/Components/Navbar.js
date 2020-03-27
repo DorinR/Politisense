156778c32008176468bf5ae9aeb97c51e9a12fcb
DorinR <dorin.rogov@gmail.com>
2020-03-27 16:30:58 -0400

    #234 Riding colors are now appearing, and clicking on them gets us the representative from that riding

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -201,7 +201,7 @@ export default function MiniDrawer ({ children }) {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      {/*<image xlinkHref={}*/}
+      {/* <image xlinkHref={} */}
       <AppBar
         position='fixed'
         className={clsx(classes.appBar, {
