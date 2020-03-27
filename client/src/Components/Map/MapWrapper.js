b691cc82147ed8d01ca9c2707ccc62d6d162061e
DorinR <dorin.rogov@gmail.com>
2020-03-27 00:18:41 -0400

    #232 zooming in out and reset works. Reset needs to be made more smooth

--- client/src/Components/Map/MapWrapper.js
+++ client/src/Components/Map/MapWrapper.js
@@ -1,59 +1,62 @@
+// eslint-disable-next-line no-unused-vars
+import mapStyling from './Map.css'
 import React, { Component } from 'react'
 import CanadaMap from './CanadaMap'
-import mapStyling from './Map.css'
+import IconButton from '@material-ui/core/IconButton'
+import AddCircleIcon from '@material-ui/icons/AddCircle'
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle'
+import CachedIcon from '@material-ui/icons/Cached'
 
 const divStyle = {
   marginTop: 70,
   marginLeft: 100
 }
-const headerStyle = {
-  marginLeft: 450,
-  fontSize: 40
-}
 
 export default class ChartWrapper extends Component {
-
-  componentDidMount () {
+  componentDidMount() {
     // eslint-disable-next-line no-new
-    new CanadaMap(this.refs.mapHolde,this.props.data)
-
+    new CanadaMap(this.refs.mapHolde, this.props.data)
   }
 
-  render () {
+  render() {
     return (
       <div>
-        <h1 style={headerStyle}> Map of Canada </h1>
-        <div style={divStyle} id='map-holder' ref='mapHolder' >
-          <div className="right-sidebar">
-            <div id="tooltip" className="hidden">
-              <div id="closebutton">
-                <button className="icon" border="0">Close</button>
+        <div style={divStyle} id='map-holder' ref='mapHolder'>
+          <div className='right-sidebar'>
+            <div id='tooltip' className='hidden'>
+              <div id='closebutton'>
+                <button className='icon' border='0'>
+                  Close
+                </button>
               </div>
-              <svg id={"testing"} width='200' height='200'> </svg>
-              <div id="value">
-              </div>
-
+              <svg id='testing' width='200' height='200'>
+                {' '}
+              </svg>
+              <div id='value' />
             </div>
           </div>
-          <div id="map-wrapper">
-
-            <div className="zoom-buttons">
-              <div className="zoom-button" id="zoomin">
-                <button className="zoom-button-icon" src="icons/plus.svg" border="0">+</button>
+          <div id='map-wrapper'>
+            <div className='zoom-buttons'>
+              <div className='zoom-button' id='zoomin'>
+                <IconButton>
+                  <AddCircleIcon fontSize='large' />
+                </IconButton>
               </div>
-              <div className="zoom-button zoombutton-out disable" id="zoomout">
-                <button className="zoom-button-icon" src="icons/minus.svg" border="0">-</button>
+              <div className='zoom-button zoombutton-out disable' id='zoomout'>
+                <IconButton>
+                  <RemoveCircleIcon fontSize='large' />
+                </IconButton>
               </div>
-              <div className="zoom-button zoombutton-out disable" id="reset">
-                <button className="zoom-button-icon" src="icons/close.svg" border="0">reset</button>
+              <div className='zoom-button zoombutton-out disable' id='reset'>
+                <IconButton>
+                  <CachedIcon fontSize='large' />
+                </IconButton>
               </div>
             </div>
-
-            <svg id="map"></svg>
-
+            <svg id='map' />
           </div>
         </div>
       </div>
-        )
+    )
   }
 }
