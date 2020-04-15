010d6cc611ab0f8f1c90cdd805a61c42f0a6c0f0
TheAlexRobert <alexandrerobertsmeets@gmail.com>
2020-04-14 22:55:26 -0400

    #244 improved loading time of page by separating useEffects

--- client/src/Components/Dashboard/Polls/Voting.js
+++ client/src/Components/Dashboard/Polls/Voting.js
@@ -21,242 +21,247 @@ import Divider from '@material-ui/core/Divider'
 import TextField from '@material-ui/core/TextField'
 
 const useStyles = makeStyles({
-    card: {
-        width: 500
-    },
-    avatar: {
-        backgroundColor: '#43D0C4'
-    },
-    title: {
-        fontSize: 18,
-        color: '#000000'
-    },
-    date: {
-        marginBottom: 16,
-        fontSize: 10
-    },
-    link: {
-        fontSize: 14,
-        color: '#1e90ff'
-    },
-    details: {
-        fontSize: 14
-    },
-    description: {
-        fontSize: 16
-    },
-    for: {
-        backgroundColor: '#008000',
-        color: 'white',
-        marginLeft: 10,
-        marginTop: 10,
-        '&:hover': {
-            backgroundColor: '#aee6ae',
-            color: 'black'
-        }
-    },
-    against: {
-        backgroundColor: '#8b0000',
-        color: 'white',
-        marginLeft: 20,
-        marginTop: 10,
-        '&:hover': {
-            backgroundColor: '#eba4a4',
-            color: 'black'
-        }
-    },
-    alert: {
-        fontSize: 14,
-        marginTop: 20,
-        marginBlock: 10
+  card: {
+    width: 500
+  },
+  avatar: {
+    backgroundColor: '#43D0C4'
+  },
+  title: {
+    fontSize: 18,
+    color: '#000000'
+  },
+  date: {
+    marginBottom: 16,
+    fontSize: 10
+  },
+  link: {
+    fontSize: 14,
+    color: '#1e90ff'
+  },
+  details: {
+    fontSize: 14
+  },
+  description: {
+    fontSize: 16
+  },
+  for: {
+    backgroundColor: '#008000',
+    color: 'white',
+    marginLeft: 10,
+    marginTop: 10,
+    '&:hover': {
+      backgroundColor: '#aee6ae',
+      color: 'black'
     }
+  },
+  against: {
+    backgroundColor: '#8b0000',
+    color: 'white',
+    marginLeft: 20,
+    marginTop: 10,
+    '&:hover': {
+      backgroundColor: '#eba4a4',
+      color: 'black'
+    }
+  },
+  alert: {
+    fontSize: 14,
+    marginTop: 20,
+    marginBlock: 10
+  }
 })
 
-async function fetchRecentBills() {
-    return axios
-        .get('http://localhost:5000/api/bills/getUpcomingBills')
-        .then(res => {
-            if (res.data.success) {
-                return res.data.data
-            }
-            return null
-        })
-        .catch(console.error)
+export async function fetchUserData (userEmail) {
+  let result = ''
+  await axios
+    .get(`http://localhost:5000/api/users/${userEmail}/getUser`,
+      { params: { changepassword: userEmail } })
+    .then(res => {
+      if (res.data.success) {
+        const user = res.data.data
+        result = user
+      }
+    })
+    .catch(err => console.log(err))
+  return result
 }
 
-export async function fetchUserData(userEmail) {
-    let result = ''
-    await axios
-        .get(`http://localhost:5000/api/users/${userEmail}/getUser`,
-            { params: { changepassword: userEmail } })
-        .then(res => {
-            if (res.data.success) {
-                const user = res.data.data
-                result = user
-            }
-        })
-        .catch(err => console.log(err))
-    return result
-}
+export default function Voting () {
+  const classes = useStyles()
+  const [recentBills, setRecentBills] = useState([])
+  const [storagePostalCode, setStoragePostalCode] = useState('')
+  const [ipPostalCode, setIpPostalCode] = useState('')
+  const preventDefault = (event) => event.preventDefault()
+  const [userEmail, setUserEmail] = useState('')
+  const [hasNotClicked, setHasNotClicked] = useState([])
+  const [filter, setFilter] = useState('')
+  const [filteredBills, setfilteredBills] = useState([])
+  const [user, setUser] = useState(null)
 
-export default function Voting() {
-    const classes = useStyles()
-    const [name] = useState('')
-    const [recentBills, setRecentBills] = useState([])
-    const [storagePostalCode, setStoragePostalCode] = useState('')
-    const [ipPostalCode, setIpPostalCode] = useState('')
-    const preventDefault = (event) => event.preventDefault()
-    const [userEmail, setUserEmail] = useState('')
-    const [hasNotClicked, setHasNotClicked] = useState([])
-    const [filter, setFilter] = useState('')
-    const [filteredBills, setfilteredBills] = useState([])
-
-    useEffect(() => {
-        async function getData() {
-            const user = JSON.parse(localStorage.getItem('user'))
-            const userDetails = await fetchUserData(user.email)
-            const postalCode = userDetails.postalCode.substring(0, 3)
-            setUserEmail(userDetails.email)
-            setStoragePostalCode(postalCode)
-            const ipAddress = await getIpPostalCode()
-            setIpPostalCode(ipAddress)
-            const recentLegislativeActivities = await fetchVotingActivity(user.email)
-            setRecentBills(recentLegislativeActivities.slice(0, 100))
-        }
-        getData()
-    }, [name])
-
-    useEffect(() => {
-        let filtered
-        if (filter === '') {
-            filtered = recentBills
-        } else {
-            filtered = recentBills.filter((bills) =>
-                bills.title.toLowerCase().includes(filter.toLowerCase())
-            )
-        }
-        setfilteredBills(filtered)
-    }, [filter, recentBills])
-
-    const handleFilterChange = (e) => {
-        setFilter(e.target.value)
+  useEffect(() => {
+    if (!user) {
+      // eslint-disable-next-line no-undef
+      const usr = JSON.parse(localStorage.getItem('user'))
+      setUser(usr)
     }
+  }, [user])
 
-    const registerButtonClick = (event, number, title, link, description, date, index) => {
-        event.preventDefault()
-        if (event.currentTarget.value === 'yes') {
-            // eslint-disable-next-line
+  useEffect(() => {
+    async function getData () {
+      const userDetails = await fetchUserData(user.email)
+      const postalCode = userDetails.postalCode.substring(0, 3)
+      setStoragePostalCode(postalCode)
+      setUserEmail(userDetails.email)
+    }
+    if (user) {
+      getData()
+    }
+  }, [user])
+
+  useEffect(() => {
+    async function getData () {
+      const ipAddress = await getIpPostalCode()
+      setIpPostalCode(ipAddress)
+      const recentLegislativeActivities = await fetchVotingActivity(user.email)
+      setRecentBills(recentLegislativeActivities.slice(0, 100))
+    }
+    if (user) {
+      getData()
+    }
+  }, [user])
+
+  useEffect(() => {
+    let filtered
+    if (filter === '') {
+      filtered = recentBills
+    } else {
+      filtered = recentBills.filter((bills) =>
+        bills.title.toLowerCase().includes(filter.toLowerCase())
+      )
+    }
+    setfilteredBills(filtered)
+  }, [filter, recentBills])
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value)
+  }
+
+  const registerButtonClick = (event, number, title, link, description, date, index) => {
+    event.preventDefault()
+    if (event.currentTarget.value === 'yes') {
+      // eslint-disable-next-line
             alert('You voted For this bill. Reload to see results')
-        }
-        if (event.currentTarget.value === 'no') {
-            // eslint-disable-next-line
+    }
+    if (event.currentTarget.value === 'no') {
+      // eslint-disable-next-line
             alert('You voted Against this bill. Reload to see results')
+    }
+
+    registerVote(userEmail, number, title, link, description, date, event.currentTarget.value, index)
+  }
+
+  async function fetchVotingActivity (userEmail) {
+    return axios
+      .post('http://localhost:5000/api/voting/index',
+        {
+          user: { email: userEmail }
+
+        })
+      .then(res => {
+        if (res.data.success) {
+          return res.data.data
         }
+        return null
+      })
+      .catch(console.error)
+  }
 
-        registerVote(userEmail, number, title, link, description, date, event.currentTarget.value, index)
-    }
+  async function registerVote (userEmail, number, title, link, description, date, vote, index) {
+    await axios.post('http://localhost:5000/api/voting/vote',
+      {
 
-    async function fetchVotingActivity(userEmail) {
-        return axios
-            .post('http://localhost:5000/api/voting/index',
-                {
-                    user: { email: userEmail }
+        user: { email: userEmail },
+        activity: { number: number, title: title, link: link, description: description, date: date },
+        vote: vote
 
-                })
-            .then(res => {
-                if (res.data.success) {
-                    return res.data.data
-                }
-                return null
-            })
-            .catch(console.error)
-    }
+      })
+      .then(res => {
+        if (res.data.success) {
+          setHasNotClicked([...hasNotClicked, index])
+        }
+      })
+      .catch(err => console.log(err))
+  }
 
-    async function registerVote(userEmail, number, title, link, description, date, vote, index) {
-        await axios.post('http://localhost:5000/api/voting/vote',
-            {
-
-                user: { email: userEmail },
-                activity: { number: number, title: title, link: link, description: description, date: date },
-                vote: vote
-
-            })
-            .then(res => {
-                if (res.data.success) {
-                    setHasNotClicked([...hasNotClicked, index])
-                }
-            })
-            .catch(err => console.log(err))
-    }
-
-    return (
-        <Grid>
-            <TextField
-                label='Filter through bills'
-                className={classes.search}
-                variant='outlined'
-                onChange={handleFilterChange}
-                color='primary'
-            />
-            {filteredBills && filteredBills.length > 0 ? (
-                <Grid item xs={12} align='center'>
-                    <List className={classes.root}>
-                        {filteredBills.map((bills, index) => (
-                            <ListItem key={index} value={bills}>
-                                <ListItemAvatar>
-                                    <Avatar src='http://www.pngall.com/wp-content/uploads/2016/07/Canada-Leaf-PNG-Clipart.png' />
-                                </ListItemAvatar>
-                                <Card className={classes.root}>
-                                    <CardContent>
-                                        <Typography className={classes.title} color='primary' gutterBottom>
-                                            {bills.title}
-                                        </Typography>
-                                        <Typography className={classes.date}>
-                                            Retrieved on {bills.date}
-                                        </Typography>
-                                        <CardActions>
-                                            <Typography className={classes.details}>
-                                                Get more details
-                                             </Typography>
-                                            <Typography className={classes.link}>
-                                                <Link href='#' onClick={preventDefault} className={classes.link}>
-                                                    {bills.link}
-                                                </Link>
-                                            </Typography>
-                                        </CardActions>
-                                        <Typography variant='body2' component='p'>
-                                            {bills.description}
-                                        </Typography>
-                                        {storagePostalCode === ipPostalCode && bills.description !== ''
-                                            ? <Alert className={classes.alert} severity='error'>
-                                                Read the bill properly. Once you cast your vote, it cannot be undone!
-                                      </Alert> : ''}
-                                        {storagePostalCode === ipPostalCode && bills.userHasVoted == false && bills.description !== ''
-                                            ? <ButtonGroup>
-                                                <Button value='yes' id={index} className={classes.for} onClick={(event) => registerButtonClick(event, bills.number, bills.title, bills.link, bills.description, bills.date, index)}>For</Button>
-                                                <Button value='no' id={index} className={classes.against} onClick={(event) => registerButtonClick(event, bills.number, bills.title, bills.link, bills.description, bills.date, index)}>Against</Button>
-                                            </ButtonGroup> : ''}
-                                        {bills.description !== '' && bills.userHasVoted == true
-                                            ? <List component='nav' className={classes.root} aria-label='mailbox folders'>
-                                                <ListItem>
-                                                    <ListItemText primary='For' /> {bills.yes}
-                                                </ListItem>
-                                                <Divider />
-                                                <ListItem>
-                                                    <ListItemText primary='Against' /> {bills.no}
-                                                </ListItem>
-                                            </List> : ''}
-                                    </CardContent>
-                                </Card>
-                            </ListItem>
-                        ))}
-                    </List>
-                </Grid>) : (
-                    <Typography variant='h5' component='h2'>
-                        No Results Found
+  return (
+    <Grid>
+      <TextField
+        label='Filter through bills'
+        className={classes.search}
+        variant='outlined'
+        onChange={handleFilterChange}
+        color='primary'
+      />
+      {filteredBills && filteredBills.length > 0 ? (
+        <Grid item xs={12} align='center'>
+          <List className={classes.root}>
+            {filteredBills.map((bills, index) => (
+              <ListItem key={index} value={bills}>
+                <ListItemAvatar>
+                  <Avatar src='http://www.pngall.com/wp-content/uploads/2016/07/Canada-Leaf-PNG-Clipart.png' />
+                </ListItemAvatar>
+                <Card className={classes.root}>
+                  <CardContent>
+                    <Typography className={classes.title} color='primary' gutterBottom>
+                      {bills.title}
                     </Typography>
-                )}
-        </Grid>
-    )
-
+                    <Typography className={classes.date}>
+                                            Retrieved on {bills.date}
+                    </Typography>
+                    <CardActions>
+                      <Typography className={classes.details}>
+                                                Get more details
+                      </Typography>
+                      <Typography className={classes.link}>
+                        <Link href='#' onClick={preventDefault} className={classes.link}>
+                          {bills.link}
+                        </Link>
+                      </Typography>
+                    </CardActions>
+                    <Typography variant='body2' component='p'>
+                      {bills.description}
+                    </Typography>
+                    {storagePostalCode === ipPostalCode && bills.description !== ''
+                      ? <Alert className={classes.alert} severity='error'>
+                                                Read the bill properly. Once you cast your vote, it cannot be undone!
+                        </Alert> : ''}
+                    {storagePostalCode === ipPostalCode && bills.userHasVoted === false && bills.description !== ''
+                      ? <ButtonGroup>
+                        <Button value='yes' id={index} className={classes.for} onClick={(event) => registerButtonClick(event, bills.number, bills.title, bills.link, bills.description, bills.date, index)}>For</Button>
+                        <Button value='no' id={index} className={classes.against} onClick={(event) => registerButtonClick(event, bills.number, bills.title, bills.link, bills.description, bills.date, index)}>Against</Button>
+                        </ButtonGroup> : ''}
+                    {bills.description !== '' && bills.userHasVoted === true
+                      ? <List component='nav' className={classes.root} aria-label='mailbox folders'>
+                        <ListItem>
+                          <ListItemText primary='For' /> {bills.yes}
+                        </ListItem>
+                        <Divider />
+                        <ListItem>
+                          <ListItemText primary='Against' /> {bills.no}
+                        </ListItem>
+                        </List> : ''}
+                  </CardContent>
+                </Card>
+              </ListItem>
+            ))}
+          </List>
+        </Grid>)
+        : (
+          <Typography variant='h5' component='h2'>
+                        No Results Found
+          </Typography>
+        )}
+    </Grid>
+  )
 }
